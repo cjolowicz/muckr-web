4be8143f30c83fc5adc742d1aad4df2ca4a7bbd5
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-12-27 18:35:54 +0100

    Add hello world server

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,12 @@
 // @flow
-/* eslint-disable no-console */
-console.log('Hello world');
+import express from 'express';
+
+const PORT = 8000;
+const app = express();
+
+app.get('/', (request, response) => response.send('Hello world'));
+
+app.listen(PORT, () => {
+  /* eslint-disable no-console */
+  console.log(`Server running on port ${PORT}.`);
+});
