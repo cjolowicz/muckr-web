df9b48ddc42564f87a6527461fcb9bbd69f989c7
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-12-27 23:23:04 +0100

    Compress response bodies

--- src/server/app.js
+++ src/server/app.js
@@ -1,8 +1,10 @@
 // @flow
 import express from 'express';
+import compression from 'compression';
 
 const app = express();
 
+app.use(compression());
 app.get('/', (request, response) => response.send('Hello world'));
 
 export default app;
