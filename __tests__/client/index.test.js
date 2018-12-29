0a0e9f28278a6b1ebe3e030cac617fdf911db460
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-12-29 13:42:56 +0100

    Add type annotations to client test

--- __tests__/client/index.test.js
+++ __tests__/client/index.test.js
@@ -1,17 +1,22 @@
+// @flow
 describe('client', () => {
   test('inserts heading', () => {
-    document.body.innerHTML = '<div class="app"></div>';
-    // eslint-disable-next-line global-require
-    require('../../src/client/index');
-    expect(document.body.innerHTML).toBe(
-      '<div class="app"><h1>Hello world</h1></div>',
-    );
+    if (document.body != null) {
+      document.body.innerHTML = '<div class="app"></div>';
+      // eslint-disable-next-line global-require
+      require('../../src/client/index');
+      expect(document.body.innerHTML).toBe(
+        '<div class="app"><h1>Hello world</h1></div>',
+      );
+    }
   });
 
   test('only modifies designated element', () => {
-    document.body.innerHTML = '<div></div>';
-    // eslint-disable-next-line global-require
-    require('../../src/client/index');
-    expect(document.body.innerHTML).toBe('<div></div>');
+    if (document.body != null) {
+      document.body.innerHTML = '<div></div>';
+      // eslint-disable-next-line global-require
+      require('../../src/client/index');
+      expect(document.body.innerHTML).toBe('<div></div>');
+    }
   });
 });
