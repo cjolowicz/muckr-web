3c542be5bb8c5b0a140b66bb747b4084421912eb
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-12-29 21:33:13 +0100

    Serve webpack bundle at /static

--- src/server/app.js
+++ src/server/app.js
@@ -2,9 +2,13 @@
 import express from 'express';
 import compression from 'compression';
 
+import { STATIC_PATH } from '../constants';
+import { WEBPACK_PATH } from '../../webpack.config.babel';
+
 const app = express();
 
 app.use(compression());
+app.use(STATIC_PATH, express.static(WEBPACK_PATH));
 app.get('/', (request, response) => response.send('Hello world'));
 
 export default app;
