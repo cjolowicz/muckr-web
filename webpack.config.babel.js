a348f6e006d33185755f5bc2765b18c37f1597ec
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-12-29 20:51:58 +0100

    Provide constants for serving webpack bundle
     - Export constant STATIC_PATH from src/constants.js.
     - Export WEBPACK_PATH and WEBPACK_LOCATION from webpack
       configuration.
    
    These constants are required to configure the server.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,19 +1,29 @@
 // @flow
 import path from 'path';
 
+import { STATIC_PATH } from './src/constants';
+
 const isProduction = process.env.NODE_ENV === 'production';
 const WEBPACK_PATH = 'dist';
+const WEBPACK_FILE = 'js/bundle.js';
 const WEBPACK_DEV_SERVER_PORT = 7000;
-const WEBPACK_DEV_SERVER_URL = `http://localhost:${WEBPACK_DEV_SERVER_PORT}/${WEBPACK_PATH}/`
+const WEBPACK_DEV_SERVER_URL = `http://localhost:${WEBPACK_DEV_SERVER_PORT}/${WEBPACK_PATH}/`;
+const WEBPACK_PUBLIC_PATH = isProduction ? STATIC_PATH : WEBPACK_DEV_SERVER_URL;
+const WEBPACK_LOCATION = `${WEBPACK_PUBLIC_PATH}${WEBPACK_FILE}`;
+
+export {
+  WEBPACK_PATH,
+  WEBPACK_LOCATION,
+};
 
 export default {
   entry: [
     './src/client/index.js',
   ],
   output: {
-    filename: 'js/bundle.js',
+    filename: WEBPACK_FILE,
     path: path.resolve(__dirname, WEBPACK_PATH),
-    publicPath: isProduction ? '/static/' : WEBPACK_DEV_SERVER_URL,
+    publicPath: WEBPACK_PUBLIC_PATH,
   },
   module: {
     rules: [
