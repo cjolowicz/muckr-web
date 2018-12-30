4431eaf560e9f7fdc282b6befaded51979f6a340
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-12-30 22:07:43 +0100

    Display API token

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,15 @@
 // @flow
 import '@babel/polyfill';
 
+import { fetchToken } from './auth';
+import { API_USER, API_PASSWORD } from '../constants';
+
 const element = document.querySelector('.app');
 
 if (element != null) {
-  element.innerHTML = '<h1>Hello world</h1>';
+  fetchToken(API_USER, API_PASSWORD).then((token) => {
+    element.innerHTML = `token: ${token}`;
+  }).catch((error) => {
+    element.innerHTML = `error: ${error}`;
+  });
 }
