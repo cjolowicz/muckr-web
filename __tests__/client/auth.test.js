cfa88330add9a42fa99c94ed7c28e62460b5225e
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-12-31 13:33:31 +0100

    Add fetchToken to client/auth

--- __tests__/client/auth.test.js
+++ __tests__/client/auth.test.js
@@ -1,5 +1,14 @@
 // @flow
-import createBasicAuthHeader from '../../src/client/auth';
+import fetchMock from 'fetch-mock';
+import { API_URL } from '../../src/constants';
+import {
+  createBasicAuthHeader,
+  fetchToken,
+} from '../../src/client/auth';
+
+afterEach(() => {
+  fetchMock.restore();
+});
 
 describe('createBasicAuthHeader', () => {
   const headers = createBasicAuthHeader('john', 'secret');
@@ -8,5 +17,17 @@ describe('createBasicAuthHeader', () => {
     expect(headers.get('Authorization')).toEqual(
       'Basic am9objpzZWNyZXQ=',
     );
+  });
+});
+
+describe('fetchToken', () => {
+  test('returns token', (done) => {
+    // an arbitrary 32-byte sequence in hexadecimal
+    const expected = 'a7e743fb7ca1c7c883312b6abb0c99131e4ff210e0730d2f1ee4fad87e514ea2';
+    fetchMock.post(`${API_URL}/tokens`, { token: expected });
+    fetchToken('john', 'secret').then((token) => {
+      expect(token).toBe(expected);
+    });
+    done();
   });
 });
--- src/client/auth.js
+++ src/client/auth.js
@@ -1,10 +1,20 @@
 // @flow
 import 'isomorphic-fetch';
+import { API_URL } from '../constants';
 
-export default function createBasicAuthHeader(username: string, password: string) {
+export function createBasicAuthHeader(username: string, password: string) {
   const base64 = btoa(`${username}:${password}`);
 
   return new Headers({
     Authorization: `Basic ${base64}`,
   });
+}
+
+export function fetchToken(username: string, password: string) {
+  return fetch(`${API_URL}/tokens`, {
+    method: 'POST',
+    mode: 'cors',
+    headers: createBasicAuthHeader(username, password),
+  }).then(response => response.json())
+    .then(data => data.token);
 }
