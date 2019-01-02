143699bb260bdd55f3526a01cbad3baa27148245
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-01-02 15:03:09 +0100

    Disable import/prefer-default-export
    Always prefer named exports, for reasons explained here:
    
     - https://blog.neufund.org/why-we-have-banned-default-exports-and-you-should-do-the-same-d51fdc2cf2ad
     - https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html

--- src/client/artists.js
+++ src/client/artists.js
@@ -3,7 +3,6 @@ import 'isomorphic-fetch';
 import { API_URL } from '../constants';
 import { createTokenAuthHeader } from './auth';
 
-// eslint-disable-next-line import/prefer-default-export
 export function fetchArtists(token: string) {
   return fetch(`${API_URL}/artists`, {
     method: 'GET',
