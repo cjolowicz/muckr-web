7fc54b993326eee10d3c2e06d02cb2238581e137
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-01-02 15:39:24 +0100

    Display artists

--- src/client/index.js
+++ src/client/index.js
@@ -2,14 +2,18 @@
 import '@babel/polyfill';
 
 import { fetchToken } from './auth';
+import { fetchArtists } from './artists';
 import { API_USER, API_PASSWORD } from '../constants';
 
 const element = document.querySelector('.app');
 
 if (element != null) {
-  fetchToken(API_USER, API_PASSWORD).then((token) => {
-    element.innerHTML = `token: ${token}`;
-  }).catch((error) => {
-    element.innerHTML = `error: ${error}`;
-  });
+  fetchToken(API_USER, API_PASSWORD)
+    .then(fetchArtists)
+    .then((artists) => {
+      element.innerHTML = JSON.stringify(artists);
+    })
+    .catch((error) => {
+      element.innerHTML = `error: ${error}`;
+    });
 }
