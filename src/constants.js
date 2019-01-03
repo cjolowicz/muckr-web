1aee8594fcdb8f392386c0d3e6d325e29a859a55
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-01-03 21:55:27 +0100

    Typecheck constants
    Flow mistakenly infers Maybe types when object destructuring is used
    with default values (#183). As a workaround, we use just and
    fromMaybe from utils.
    
      https://github.com/facebook/flow/issues/183
    
    Note that dotenv-webpack requires us to reference process.env
    properties explicitly. The following will not work:
    
      export const API_URL = env('API_URL')

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,20 @@
-export const PRODUCTION = process.env.NODE_ENV === 'production';
+// @flow
+import {
+  just,
+  fromMaybe,
+} from './utils';
 
+export const API_URL = just(process.env.API_URL);
+export const API_USER = just(process.env.API_USER);
+export const API_PASSWORD = just(process.env.API_PASSWORD);
+export const NODE_ENV = fromMaybe('development', process.env.NODE_ENV);
+export const PORT = fromMaybe('8000', process.env.PORT);
+export const PRODUCTION = NODE_ENV === 'production';
+export const APP_ROOT = 'root';
 export const STATIC_PATH = '/static';
-
 export const WEBPACK_PATH = 'dist';
 export const WEBPACK_FILE = 'js/bundle.js';
 export const WEBPACK_DEV_SERVER_PORT = 7000;
 export const WEBPACK_DEV_SERVER_URL = `http://localhost:${WEBPACK_DEV_SERVER_PORT}/${WEBPACK_PATH}`;
 export const WEBPACK_PUBLIC_PATH = PRODUCTION ? STATIC_PATH : WEBPACK_DEV_SERVER_URL;
 export const WEBPACK_LOCATION = `${WEBPACK_PUBLIC_PATH}/${WEBPACK_FILE}`;
-
-export const { API_URL } = process.env;
-export const { API_USER } = process.env;
-export const { API_PASSWORD } = process.env;
-export const { NODE_ENV = 'development' } = process.env;
-export const { PORT = 8000 } = process.env;
-
-export const APP_ROOT = 'root';
