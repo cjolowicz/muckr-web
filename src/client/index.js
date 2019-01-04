58705a1031f9632b733c620fb312776da386c1ad
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-01-04 14:30:25 +0100

    Error out if root element is missing

--- src/client/index.js
+++ src/client/index.js
@@ -1,19 +1,18 @@
 // @flow
 import '@babel/polyfill';
 
+import { just } from '../utils';
 import { fetchToken } from './auth';
 import { fetchArtists } from './artists';
 import { API_USER, API_PASSWORD, APP_ROOT } from '../constants';
 
-const element = document.querySelector(`#${APP_ROOT}`);
+const element = just(document.querySelector(`#${APP_ROOT}`));
 
-if (element != null) {
-  fetchToken(API_USER, API_PASSWORD)
-    .then(fetchArtists)
-    .then((artists) => {
-      element.innerHTML = `<ul>${artists.map(artist => `<li>${artist.name}</li>`).join('')}</ul>`;
-    })
-    .catch((error) => {
-      element.innerHTML = `error: ${error}`;
-    });
-}
+fetchToken(API_USER, API_PASSWORD)
+  .then(fetchArtists)
+  .then((artists) => {
+    element.innerHTML = `<ul>${artists.map(artist => `<li>${artist.name}</li>`).join('')}</ul>`;
+  })
+  .catch((error) => {
+    element.innerHTML = `error: ${error}`;
+  });
