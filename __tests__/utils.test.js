56f2c2da16523e52857c3b2484359e8133c1f615
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-01-04 16:51:52 +0100

    Add just to utils

---
+++ __tests__/utils.test.js
@@ -0,0 +1,29 @@
+// @flow
+import { just } from '../src/utils';
+
+describe('just', () => {
+  test.each([
+    [undefined],
+    [null],
+  ])('throws if undefined or null', (arg) => {
+    expect(() => just(arg)).toThrow(Error);
+  });
+
+  test.each([
+    [false],
+    [true],
+    [0],
+    [1],
+    [2],
+    [''],
+    ['foo'],
+    [[]],
+    [['foo']],
+    [{}],
+    [{ foo: 'foo' }],
+    [() => null],
+    [() => undefined],
+  ])('returns argument', (arg) => {
+    expect(just(arg)).toBe(arg);
+  });
+});
---
+++ src/utils.js
@@ -0,0 +1,12 @@
+// @flow
+export function just<T>(value: ?T): T {
+  if (value === undefined) {
+    throw new Error('value is undefined');
+  }
+
+  if (value === null) {
+    throw new Error('value is null');
+  }
+
+  return value;
+}
