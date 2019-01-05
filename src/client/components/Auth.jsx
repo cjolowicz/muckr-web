13f92ea57376ae398f622fa8ab14e0f8476a1f39
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-01-05 23:58:04 +0100

    Add withAuth higher-order component

---
+++ src/client/components/Auth.jsx
@@ -0,0 +1,72 @@
+// @flow
+import * as React from 'react';
+
+import { fetchToken } from '../auth';
+
+type Props = {
+  username: string,
+  password: string,
+};
+
+type State = {
+  token: ?string,
+  error: ?Error,
+  isLoading: boolean,
+};
+
+function getDisplayName(Component) {
+  return Component.displayName || Component.name || 'Component';
+}
+
+export function withAuth(
+  Component: React.ComponentType<{ token: string }>,
+): React.ComponentType<Props> {
+  class WithAuth extends React.Component<Props, State> {
+    constructor(props: Props) {
+      super(props);
+
+      this.state = {
+        token: null,
+        error: null,
+        isLoading: false,
+      };
+    }
+
+    componentDidMount() {
+      this.setState({ isLoading: true });
+
+      const { username, password } = this.props;
+
+      fetchToken(username, password)
+        .then(token => this.setState({
+          token,
+          isLoading: false,
+        })).catch(error => this.setState({
+          error,
+          isLoading: false,
+        }));
+    }
+
+    render() {
+      const { token, error, isLoading } = this.state;
+
+      if (error) {
+        return <p>error: {error.message}</p>;
+      }
+
+      if (isLoading) {
+        return <p>Logging in...</p>;
+      }
+
+      if (!token) {
+        return <p>no token</p>;
+      }
+
+      return <Component token={token} />;
+    }
+  }
+
+  WithAuth.displayName = `WithAuth(${getDisplayName(Component)})`;
+
+  return WithAuth;
+}
---
+++ src/client/components/__tests__/Auth.jsx
@@ -0,0 +1,111 @@
+// @flow
+import React from 'react';
+import { mount } from 'enzyme';
+
+import { withAuth } from '../Auth';
+import * as auth from '../../auth';
+import { mock } from '../../../test-utils';
+
+export const Auth = withAuth(
+  (props: { token: string }) => <p>Logged in: {props.token}</p>,
+);
+
+describe('Auth', () => {
+  describe('on success', () => {
+    const TOKEN = 'a7e743fb7ca1c7c883312b6abb0c99131e4ff210e0730d2f1ee4fad87e514ea2';
+    const promise = Promise.resolve(TOKEN);
+
+    beforeAll(() => {
+      jest.spyOn(auth, 'fetchToken').mockReturnValue(promise);
+    });
+
+    afterAll(() => {
+      mock(auth.fetchToken).mockRestore();
+    });
+
+    it('stores token', () => {
+      const wrapper = mount(<Auth username="john" password="secret" />);
+
+      expect(wrapper.state().token).toBeNull();
+
+      return promise.then(() => {
+        wrapper.update();
+        expect(wrapper.state().token).toEqual(TOKEN);
+      });
+    });
+
+    it('tracks progress', () => {
+      const wrapper = mount(<Auth username="john" password="secret" />);
+
+      expect(wrapper.state().isLoading).toBeTruthy();
+
+      return promise.then(() => {
+        wrapper.update();
+        expect(wrapper.state().isLoading).toBeFalsy();
+      });
+    });
+
+    it('renders token', () => {
+      const wrapper = mount(<Auth username="john" password="secret" />);
+
+      expect(wrapper.find('p').text()).toEqual('Logging in...');
+
+      return promise.then(() => {
+        wrapper.update();
+        expect(wrapper.find('p').text()).toEqual(`Logged in: ${TOKEN}`);
+      });
+    });
+  });
+
+  describe('on error', () => {
+    const error = new Error('fail');
+    const promise = Promise.reject(error);
+
+    beforeAll(() => {
+      jest.spyOn(auth, 'fetchToken').mockReturnValue(promise);
+    });
+
+    afterAll(() => {
+      mock(auth.fetchToken).mockRestore();
+    });
+
+    it('stores error', () => {
+      expect.assertions(2);
+
+      const wrapper = mount(<Auth username="john" password="secret" />);
+
+      expect(wrapper.state().error).toBeNull();
+
+      return promise.then().catch(() => {
+        wrapper.update();
+        expect(wrapper.state().error).toBe(error);
+      });
+    });
+
+    it('tracks progress', () => {
+      expect.assertions(2);
+
+      const wrapper = mount(<Auth username="john" password="secret" />);
+
+      expect(wrapper.state().isLoading).toBeTruthy();
+
+      return promise.then().catch(() => {
+        wrapper.update();
+        expect(wrapper.state().isLoading).toBeFalsy();
+      });
+    });
+
+    it('renders error', () => {
+      expect.assertions(2);
+
+      const wrapper = mount(<Auth username="john" password="secret" />);
+
+      expect(wrapper.find('p').text()).toEqual('Logging in...');
+
+      return promise.then().catch(() => {
+        wrapper.update();
+        expect(wrapper.find('p').text()).toEqual(`error: ${error.message}`);
+      });
+    });
+  });
+});
