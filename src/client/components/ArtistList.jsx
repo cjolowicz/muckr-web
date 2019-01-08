59db654b7a6e461c4b27b775090d8cd91efb7a6e
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-01-08 19:27:05 +0100

    Add ArtistList component

---
+++ src/client/components/ArtistList.jsx
@@ -0,0 +1,71 @@
+// @flow
+import React from 'react';
+
+import { fetchArtists } from '../artists';
+
+type Props = {
+  token: string,
+};
+
+type Artist = {
+  id: number,
+  name: string,
+};
+
+type State = {
+  artists: Array<Artist>,
+  error: ?Error,
+  isLoading: boolean,
+};
+
+export class ArtistList extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      artists: [],
+      error: null,
+      isLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+
+    const { token } = this.props;
+
+    fetchArtists(token)
+      .then(artists => this.setState({
+        artists,
+        isLoading: false,
+      }))
+      .catch(error => this.setState({
+        error,
+        isLoading: false,
+      }));
+  }
+
+  render() {
+    const { artists, error, isLoading } = this.state;
+
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    if (!artists) {
+      return <p>No artists</p>;
+    }
+
+    return (
+      <ul>
+        {artists.map(artist => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
+    );
+  }
+}
---
+++ src/client/components/__tests__/ArtistList.jsx
@@ -0,0 +1,34 @@
+// @flow
+import React from 'react';
+import { mount } from 'enzyme';
+
+import { ArtistList } from '../ArtistList';
+import * as artists from '../../artists';
+import { mock } from '../../../test-utils';
+
+describe('ArtistList', () => {
+  describe('on success', () => {
+    const token = 'a7e743fb7ca1c7c883312b6abb0c99131e4ff210e0730d2f1ee4fad87e514ea2';
+    const artist = { id: 1, name: 'Artist' };
+    const promise = Promise.resolve([artist]);
+
+    beforeAll(() => {
+      jest.spyOn(artists, 'fetchArtists').mockReturnValue(promise);
+    });
+
+    afterAll(() => {
+      mock(artists.fetchArtists).mockRestore();
+    });
+
+    test('stores artists', () => {
+      const wrapper = mount(<ArtistList token={token} />);
+
+      expect(wrapper.state().artists).toEqual([]);
+
+      return promise.then(() => {
+        wrapper.update();
+        expect(wrapper.state().artists).toEqual([artist]);
+      });
+    });
+  });
+});
