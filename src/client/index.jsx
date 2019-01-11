2a4fc6ce143f680c040d1f342ba2cf7bfb033619
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-01-11 21:00:59 +0100

    Use App component in client/index

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,10 +1,12 @@
 // @flow
 import '@babel/polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import { just } from '../utils';
 import { APP_ROOT } from '../constants';
-import { renderApp } from './app';
+import { App } from './components/App';
 
 const root = just(document.querySelector(`#${APP_ROOT}`));
 
-renderApp(root);
+ReactDOM.render(<App />, root);
