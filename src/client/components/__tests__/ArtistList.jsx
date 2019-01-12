c9814fec1f19c788fa857a6ed8259d8f36d29d3a
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-01-12 21:02:50 +0100

    Provide full test coverage for ArtistList.

--- src/client/components/__tests__/ArtistList.jsx
+++ src/client/components/__tests__/ArtistList.jsx
@@ -8,19 +8,21 @@ import { mock } from "../../../test/utils";
 import { TOKEN } from "../../../test/fixtures";
 
 describe("ArtistList", () => {
+  beforeAll(() => {
+    jest.spyOn(artists, "fetchArtists");
+  });
+
+  afterAll(() => {
+    mock(artists.fetchArtists).mockRestore();
+  });
+
   describe("on success", () => {
-    const artist = { id: 1, name: "Artist" };
-    const promise = Promise.resolve([artist]);
+    it("stores artists", () => {
+      const artist = { id: 1, name: "Artist" };
+      const promise = Promise.resolve([artist]);
 
-    beforeAll(() => {
-      jest.spyOn(artists, "fetchArtists").mockReturnValue(promise);
-    });
+      mock(artists.fetchArtists).mockReturnValue(promise);
 
-    afterAll(() => {
-      mock(artists.fetchArtists).mockRestore();
-    });
-
-    test("stores artists", () => {
       const wrapper = mount(<ArtistList token={TOKEN} />);
 
       expect(wrapper.state().artists).toEqual([]);
@@ -28,6 +30,41 @@ describe("ArtistList", () => {
       return promise.then(() => {
         wrapper.update();
         expect(wrapper.state().artists).toEqual([artist]);
+      });
+    });
+
+    it("renders message if no artists", () => {
+      const promise = Promise.resolve([]);
+
+      mock(artists.fetchArtists).mockReturnValue(promise);
+
+      const wrapper = mount(<ArtistList token={TOKEN} />);
+
+      expect(wrapper.find("p").text()).toEqual("Loading...");
+
+      return promise.then(() => {
+        wrapper.update();
+        expect(wrapper.find("p").text()).toEqual("No artists");
+      });
+    });
+  });
+
+  describe("on error", () => {
+    it("stores error", () => {
+      const error = new Error("fail");
+      const promise = Promise.reject(error);
+
+      mock(artists.fetchArtists).mockReturnValue(promise);
+
+      expect.assertions(2);
+
+      const wrapper = mount(<ArtistList token={TOKEN} />);
+
+      expect(wrapper.state().error).toBeNull();
+
+      return promise.then().catch(() => {
+        wrapper.update();
+        expect(wrapper.state().error).toBe(error);
       });
     });
   });
