21fbe432955a6ecc97a3c887c5fcfcf4c1e39ead
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-01-13 13:26:27 +0100

    Add routing
    Closes: #5
    Closes: #10

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,5 +1,6 @@
 // @flow
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 import { withAuth } from "./Auth";
 import { ArtistList } from "./ArtistList";
@@ -7,6 +8,30 @@ import { API_USER, API_PASSWORD } from "../../constants";
 
 const ArtistListWithAuth = withAuth(ArtistList);
 
-export function App() {
-  return <ArtistListWithAuth username={API_USER} password={API_PASSWORD} />;
-}
+const Index = () => <h2>Welcome</h2>;
+const Artists = () => (
+  <ArtistListWithAuth username={API_USER} password={API_PASSWORD} />
+);
+
+const Header = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/artists/">Artists</Link>
+      </li>
+    </ul>
+  </nav>
+);
+
+export const App = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Route path="/" exact component={Index} />
+      <Route path="/artists/" component={Artists} />
+    </div>
+  </BrowserRouter>
+);
