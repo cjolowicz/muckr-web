6e3629d6d63e435f5cc5364a3ef9703f888c979d
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-01-13 22:01:59 +0100

    Add LoginForm component

---
+++ src/client/components/LoginForm.jsx
@@ -0,0 +1,85 @@
+// @flow
+import React from "react";
+import type { ContextRouter } from "react-router";
+
+import { withRouter } from "react-router-dom";
+
+import { fetchToken } from "../auth";
+
+type Props = ContextRouter & {
+  nextRoute: string
+};
+
+type State = {
+  username: string,
+  password: string,
+  error: ?Error,
+  isLoading: boolean
+};
+
+export class LoginFormBase extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = { username: "", password: "", error: null, isLoading: false };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange: () => void;
+
+  handleChange(event: SyntheticEvent<HTMLInputElement>) {
+    this.setState({ [event.currentTarget.name]: event.currentTarget.value });
+  }
+
+  handleSubmit: () => void;
+
+  handleSubmit(event: SyntheticEvent<HTMLButtonElement>) {
+    const { history, nextRoute } = this.props;
+    const { username, password } = this.state;
+
+    this.setState({ isLoading: true });
+
+    fetchToken(username, password)
+      .then(token => {
+        localStorage.setItem("token", token);
+        this.setState({ isLoading: false });
+        history.push(nextRoute);
+      })
+      .catch(error => {
+        this.setState({ error, isLoading: false });
+      });
+
+    event.preventDefault();
+  }
+
+  render() {
+    const { username, password, error, isLoading } = this.state;
+    const disabled = !username.length || !password.length;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name="username"
+          type="text"
+          value={username}
+          placeholder="username"
+          onChange={this.handleChange}
+        />
+        <input
+          name="password"
+          type="password"
+          value={password}
+          placeholder="password"
+          onChange={this.handleChange}
+        />
+        <input type="submit" value="Sign in" disabled={disabled} />
+        {error ? <p>{error.message}</p> : null}
+        {isLoading ? <p>Logging in...</p> : null}
+      </form>
+    );
+  }
+}
+
+export const LoginForm = withRouter(LoginFormBase);
---
+++ src/client/components/__tests__/LoginForm.jsx
@@ -0,0 +1,72 @@
+// @flow
+import React from "react";
+import { mount, shallow } from "enzyme";
+
+import { LoginFormBase } from "../LoginForm";
+import * as auth from "../../auth";
+import { mock } from "../../../test/utils";
+import { TOKEN } from "../../../test/fixtures";
+
+describe("LoginForm", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  describe("on startup", () => {
+    it("displays form", () => {
+      const loginForm = shallow(
+        <LoginFormBase history={window.history} nextRoute="/" />
+      );
+      expect(loginForm.find("form")).toHaveLength(1);
+    });
+  });
+
+  describe("on change", () => {
+    it("updates state", () => {
+      const loginForm = shallow(
+        <LoginFormBase history={window.history} nextRoute="/" />
+      );
+      const usernameField = loginForm.find("input[name='username']");
+
+      expect(loginForm.state().username).toEqual("");
+
+      usernameField.simulate("change", {
+        currentTarget: {
+          name: "username",
+          value: "john"
+        }
+      });
+
+      expect(loginForm.state().username).toEqual("john");
+    });
+  });
+
+  describe("on submit", () => {
+    const promise = Promise.resolve(TOKEN);
+
+    beforeAll(() => {
+      jest.spyOn(auth, "fetchToken").mockReturnValue(promise);
+    });
+
+    afterAll(() => {
+      mock(auth.fetchToken).mockRestore();
+    });
+
+    it("stores token", () => {
+      const loginForm = mount(
+        <LoginFormBase history={window.history} nextRoute="/" />
+      );
+
+      expect(localStorage.getItem("token")).toBeNull();
+
+      loginForm.simulate("submit", {
+        preventDefault: () => {}
+      });
+
+      return promise.then(() => {
+        loginForm.update();
+        expect(localStorage.getItem("token")).toEqual(TOKEN);
+      });
+    });
+  });
+});
