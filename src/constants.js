8a832b69cff108b8222422f1915d5eee3c52c28d
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-01-15 15:56:52 +0100

    Remove API_USER and API_PASSWORD constants

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,6 @@
 import { just, fromMaybe } from "./utils";
 
 export const API_URL = just(process.env.API_URL);
-export const API_USER = just(process.env.API_USER);
-export const API_PASSWORD = just(process.env.API_PASSWORD);
 export const NODE_ENV = fromMaybe("development", process.env.NODE_ENV);
 export const PORT = fromMaybe("8000", process.env.PORT);
 export const PRODUCTION = NODE_ENV === "production";
