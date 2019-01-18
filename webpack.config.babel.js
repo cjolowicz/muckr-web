028ab77a2039b2263e73732eb3d86ec83a048199
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-01-18 21:35:25 +0100

    Configure webpack to use style-loader and css-loader

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -24,6 +24,10 @@ export default {
         test: /\.js$/,
         use: "babel-loader",
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
       }
     ]
   },
