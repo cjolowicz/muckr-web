52e753e831c4b6781c3225bb64fb9d6121a984e3
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-01-18 21:55:02 +0100

    Add Navigation component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 // @flow
 import React from "react";
-import { Link, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
+import { Navigation } from "./Navigation";
 import { withAuth } from "./Auth";
 import { ArtistList } from "./ArtistList";
 import { LoginForm } from "./LoginForm";
@@ -13,22 +14,6 @@ const Index = () => <h2>Welcome</h2>;
 const Login = () => <LoginForm nextRoute={routes.ARTISTS} />;
 const Artists = () => <ArtistListWithAuth />;
 
-const Header = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link to={routes.INDEX}>Home</Link>
-      </li>
-      <li>
-        <Link to={routes.LOGIN}>Login</Link>
-      </li>
-      <li>
-        <Link to={routes.ARTISTS}>Artists</Link>
-      </li>
-    </ul>
-  </nav>
-);
-
 const Routes = () => (
   <Switch>
     <Route path={routes.INDEX} exact component={Index} />
@@ -39,7 +24,7 @@ const Routes = () => (
 
 export const App = () => (
   <>
-    <Header />
+    <Navigation />
     <Routes />
   </>
 );
---
+++ src/components/Navigation.js
@@ -0,0 +1,20 @@
+// @flow
+import React from "react";
+import { Link } from "react-router-dom";
+import * as routes from "../routes";
+
+export const Navigation = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link to={routes.INDEX}>Home</Link>
+      </li>
+      <li>
+        <Link to={routes.LOGIN}>Login</Link>
+      </li>
+      <li>
+        <Link to={routes.ARTISTS}>Artists</Link>
+      </li>
+    </ul>
+  </nav>
+);
