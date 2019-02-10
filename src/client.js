f290466cc6821f1d779575bddcdc29faeedbe074
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-10 10:59:22 +0100

    Fix missing stylesheet when server-side rendering
    https://material-ui.com/guides/server-rendering/
    
    Closes: #36

--- src/client.js
+++ src/client.js
@@ -4,16 +4,45 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
+import { JssProvider } from "react-jss";
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  createGenerateClassName
+} from "@material-ui/core/styles";
 
 import { just } from "./utils";
 import { APP_ROOT } from "./constants";
 import { App } from "./components/App";
 
+class Main extends React.Component<{}> {
+  componentDidMount() {
+    const jss = document.getElementById("jss-server-side");
+    if (jss && jss.parentNode) {
+      jss.parentNode.removeChild(jss);
+    }
+  }
+
+  render() {
+    return <App />;
+  }
+}
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true
+  }
+});
+const generateClassName = createGenerateClassName();
 const root = just(document.querySelector(`#${APP_ROOT}`));
 const jsx = (
   <BrowserRouter>
     <CookiesProvider>
-      <App />
+      <JssProvider generateClassName={generateClassName}>
+        <MuiThemeProvider theme={theme}>
+          <Main />
+        </MuiThemeProvider>
+      </JssProvider>
     </CookiesProvider>
   </BrowserRouter>
 );
--- src/server/app.js
+++ src/server/app.js
@@ -6,6 +6,13 @@ import React from "react";
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
+import { SheetsRegistry } from "jss";
+import { JssProvider } from "react-jss";
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  createGenerateClassName
+} from "@material-ui/core/styles";
 
 import { App } from "../components/App";
 import {
@@ -15,7 +22,7 @@ import {
   WEBPACK_LOCATION
 } from "../constants";
 
-const generateHTML = jsx => `<!doctype html>
+const generatePage = (html, css) => `<!doctype html>
 <html>
   <head>
     <meta charset="utf-8">
@@ -23,9 +30,10 @@ const generateHTML = jsx => `<!doctype html>
       name="viewport"
       content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
     >
+    <style id="jss-server-side">${css}</style>
   </head>
   <body>
-    <div id="${APP_ROOT}">${renderToString(jsx)}</div>
+    <div id="${APP_ROOT}">${html}</div>
     <script src="${WEBPACK_LOCATION}"></script>
   </body>
 </html>`;
@@ -36,15 +44,32 @@ app.use(compression());
 app.use(STATIC_PATH, express.static(WEBPACK_DIR));
 app.use(cookiesMiddleware());
 app.get("/*", (request, response: express$Response) => {
+  const sheetsRegistry = new SheetsRegistry();
+  const sheetsManager = new Map();
+  const theme = createMuiTheme({
+    typography: {
+      useNextVariants: true
+    }
+  });
+  const generateClassName = createGenerateClassName();
   const jsx = (
     <StaticRouter context={{}} location={request.url}>
       <CookiesProvider cookies={request.universalCookies}>
-        <App />
+        <JssProvider
+          registry={sheetsRegistry}
+          generateClassName={generateClassName}
+        >
+          <MuiThemeProvider theme={theme} sheetsManager={sheetsManager}>
+            <App />
+          </MuiThemeProvider>
+        </JssProvider>
       </CookiesProvider>
     </StaticRouter>
   );
-  const html = generateHTML(jsx);
-  return response.send(html);
+  const html = renderToString(jsx);
+  const css = sheetsRegistry.toString();
+  const page = generatePage(html, css);
+  return response.send(page);
 });
 
 export default app;
