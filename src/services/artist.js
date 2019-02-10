4dc640c2dba0a3727b517334732028c7d03b3e8e
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-11 00:14:49 +0100

    Re-enable import/prefer-default-export

--- src/services/artist.js
+++ src/services/artist.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { API_URL } from "../constants";
 import { createTokenAuthHeader } from "./user";
 
+// eslint-disable-next-line import/prefer-default-export
 export async function fetchArtists(token: string) {
   const response = await axios.get(`${API_URL}/artists`, {
     headers: createTokenAuthHeader(token)
--- src/test/fixtures.js
+++ src/test/fixtures.js
@@ -1,3 +1,4 @@
 // an arbitrary 32-byte sequence in hexadecimal
+// eslint-disable-next-line import/prefer-default-export
 export const TOKEN =
   "a7e743fb7ca1c7c883312b6abb0c99131e4ff210e0730d2f1ee4fad87e514ea2";
