fdab034dc796d2a6ed2c75656173e9bef0c3cd19
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-11 00:54:20 +0100

    Move generatePage to separate module

---
+++ src/server/generatePage.js
@@ -0,0 +1,20 @@
+// @flow
+import { APP_ROOT, JSS_STYLE_ID, WEBPACK_LOCATION } from "../constants";
+
+export default function generatePage(html: string, css: string) {
+  return `<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta
+      name="viewport"
+      content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+    >
+    <style id="${JSS_STYLE_ID}">${css}</style>
+  </head>
+  <body>
+    <div id="${APP_ROOT}">${html}</div>
+    <script src="${WEBPACK_LOCATION}"></script>
+  </body>
+</html>`;
+}
--- src/server/render.js
+++ src/server/render.js
@@ -6,23 +6,7 @@ import { Cookies } from "react-cookie";
 import { createGenerateClassName } from "@material-ui/core/styles";
 
 import Root from "./Root";
-import { APP_ROOT, JSS_STYLE_ID, WEBPACK_LOCATION } from "../constants";
-
-const generatePage = (html, css) => `<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta
-      name="viewport"
-      content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-    >
-    <style id="${JSS_STYLE_ID}">${css}</style>
-  </head>
-  <body>
-    <div id="${APP_ROOT}">${html}</div>
-    <script src="${WEBPACK_LOCATION}"></script>
-  </body>
-</html>`;
+import generatePage from "./generatePage";
 
 type Request = express$Request & {
   universalCookies: Cookies
