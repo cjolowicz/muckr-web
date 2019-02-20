c0824232b899b041cd2bd7081581c22911353557
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-20 20:23:35 +0100

    getInstance: Remove unused test utility

--- src/test/utils.js
+++ src/test/utils.js
@@ -1,11 +1,5 @@
 // @flow
 import type { JestMockT } from "jest";
-import type { ShallowWrapper, ReactWrapper } from "enzyme";
 
-import { unsafeCast } from "../utils";
-
+// eslint-disable-next-line import/prefer-default-export
 export const mock = (fn: any): JestMockT => fn;
-
-export const getInstance = <T>(
-  wrapper: ShallowWrapper<*> | ReactWrapper<*>
-): T => unsafeCast<T>(wrapper.instance());
