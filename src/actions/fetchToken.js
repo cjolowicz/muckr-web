935a214e0a26f87e7ffa3427905202d97d16e9b4
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-21 22:36:28 +0100

    Add type FetchTokenAction

--- src/actions/fetchToken.js
+++ src/actions/fetchToken.js
@@ -1,27 +1,55 @@
 // @flow
-import type { Dispatch } from "redux";
-
 import * as user from "../api/user";
 
 export const FETCH_TOKEN_REQUEST = "FETCH_TOKEN_REQUEST";
 export const FETCH_TOKEN_SUCCESS = "FETCH_TOKEN_SUCCESS";
 export const FETCH_TOKEN_FAILURE = "FETCH_TOKEN_FAILURE";
 
-export const fetchTokenRequest = (username: string, password: string) => ({
+export type FetchTokenRequestAction = {
+  type: typeof FETCH_TOKEN_REQUEST,
+  username: string,
+  password: string
+};
+
+export type FetchTokenSuccessAction = {
+  type: typeof FETCH_TOKEN_SUCCESS,
+  token: string
+};
+
+export type FetchTokenFailureAction = {
+  type: typeof FETCH_TOKEN_FAILURE,
+  error: user.$FetchError
+};
+
+export type FetchTokenAction =
+  | FetchTokenRequestAction
+  | FetchTokenSuccessAction
+  | FetchTokenFailureAction;
+
+export const fetchTokenRequest = (
+  username: string,
+  password: string
+): FetchTokenRequestAction => ({
   type: FETCH_TOKEN_REQUEST,
   username,
   password
 });
 
-export const fetchTokenSuccess = (token: string) => ({
+export const fetchTokenSuccess = (token: string): FetchTokenSuccessAction => ({
   type: FETCH_TOKEN_SUCCESS,
   token
 });
 
-export const fetchTokenFailure = (error: user.$FetchError) => ({
+export const fetchTokenFailure = (
+  error: user.$FetchError
+): FetchTokenFailureAction => ({
   type: FETCH_TOKEN_FAILURE,
   error
 });
+
+// eslint-disable-next-line no-use-before-define
+type ThunkAction = Dispatch => any;
+export type Dispatch = (FetchTokenAction | ThunkAction) => any;
 
 export const fetchToken = (username: string, password: string) => (
   dispatch: Dispatch
