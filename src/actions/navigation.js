aa79f1754f8905aee5028f4bca047c170be359e9
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-21 10:34:45 +0100

    Add type NavigationAction

--- src/actions/navigation.js
+++ src/actions/navigation.js
@@ -2,10 +2,20 @@
 export const OPEN_NAVIGATION = "OPEN_NAVIGATION";
 export const CLOSE_NAVIGATION = "CLOSE_NAVIGATION";
 
-export const openNavigation = () => ({
+export type OpenNavigationAction = {
+  type: typeof OPEN_NAVIGATION
+};
+
+export type CloseNavigationAction = {
+  type: typeof CLOSE_NAVIGATION
+};
+
+export type NavigationAction = OpenNavigationAction | CloseNavigationAction;
+
+export const openNavigation = (): OpenNavigationAction => ({
   type: OPEN_NAVIGATION
 });
 
-export const closeNavigation = () => ({
+export const closeNavigation = (): CloseNavigationAction => ({
   type: CLOSE_NAVIGATION
 });
