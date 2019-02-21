0e302406b55d96b2ec979b93706483449c6b5eaa
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-21 10:40:01 +0100

    Add type Action

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+// @flow
+import type { MessageAction } from "./message";
+import type { NavigationAction } from "./navigation";
+import type { NoopAction } from "./noop";
+import type { FetchTokenAction } from "./fetchToken";
+import type { FetchArtistsAction } from "./fetchArtists";
+
+export type Action =
+  | NoopAction
+  | MessageAction
+  | NavigationAction
+  | FetchTokenAction
+  | FetchArtistsAction;
