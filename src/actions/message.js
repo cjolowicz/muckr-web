f8aa13b1de909246d4610bde3d71bb6be51d75cc
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-22 17:09:09 +0100

    Add type MessageAction

--- src/actions/message.js
+++ src/actions/message.js
@@ -2,11 +2,22 @@
 export const OPEN_MESSAGE = "OPEN_MESSAGE";
 export const CLOSE_MESSAGE = "CLOSE_MESSAGE";
 
-export const openMessage = (message: string) => ({
+export type OpenMessageAction = {
+  type: typeof OPEN_MESSAGE,
+  message: string
+};
+
+export type CloseMessageAction = {
+  type: typeof CLOSE_MESSAGE
+};
+
+export type MessageAction = OpenMessageAction | CloseMessageAction;
+
+export const openMessage = (message: string): OpenMessageAction => ({
   type: OPEN_MESSAGE,
   message
 });
 
-export const closeMessage = () => ({
+export const closeMessage = (): CloseMessageAction => ({
   type: CLOSE_MESSAGE
 });
