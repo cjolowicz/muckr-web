81beb33299e50045644186d6152f490a759c5cbe
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-22 16:05:20 +0100

    Test persistToken: Remove outer describe block

--- src/store/__tests__/persistToken.js
+++ src/store/__tests__/persistToken.js
@@ -11,107 +11,105 @@ import {
 } from "../../actions/fetchToken";
 import { TOKEN } from "../../test/fixtures";
 
-describe("persistToken", () => {
-  const cookies = new Cookies();
+const cookies = new Cookies();
 
-  describe("loadToken", () => {
-    const mockStore = configureStore([]);
-    let store;
+describe("loadToken", () => {
+  const mockStore = configureStore([]);
+  let store;
 
-    describe("without token", () => {
-      beforeEach(() => {
-        store = mockStore(initialState);
-        cookies.remove("token");
-        loadToken(store, cookies);
-      });
-
-      it("is no-op", () => {
-        const actions = store.getActions();
-        expect(actions).toHaveLength(0);
-      });
-    });
-
-    describe("with token", () => {
-      beforeEach(() => {
-        store = mockStore(initialState);
-        cookies.set("token", TOKEN);
-        loadToken(store, cookies);
-      });
-
-      it("dispatches an action", () => {
-        const actions = store.getActions();
-        expect(actions).toHaveLength(1);
-      });
-
-      it("dispatches FETCH_TOKEN_SUCCESS", () => {
-        const [action] = store.getActions();
-        expect(action.type).toEqual(FETCH_TOKEN_SUCCESS);
-      });
-
-      it("dispatches an action with loaded token", () => {
-        const [action] = store.getActions();
-        if (action.type === FETCH_TOKEN_SUCCESS) {
-          expect(action.token).toEqual(TOKEN);
-        }
-      });
-    });
-  });
-
-  describe("saveToken", () => {
-    let store;
-
+  describe("without token", () => {
     beforeEach(() => {
+      store = mockStore(initialState);
       cookies.remove("token");
-      store = createStore(rootReducer);
-      saveToken(store, cookies);
+      loadToken(store, cookies);
     });
 
-    describe("initially", () => {
-      it("does not set cookie", () => {
-        const token = cookies.get("token");
-        expect(token).toBeFalsy();
-      });
-    });
-
-    describe("after unrelated action", () => {
-      beforeEach(() => {
-        store.dispatch({
-          type: "test"
-        });
-      });
-
-      it("does not set cookie", () => {
-        const token = cookies.get("token");
-        expect(token).toBeFalsy();
-      });
-    });
-
-    describe("after token is fetched", () => {
-      beforeEach(() => {
-        store.dispatch(fetchTokenSuccess(TOKEN));
-      });
-
-      it("sets cookie", () => {
-        const token = cookies.get("token");
-        expect(token).toEqual(TOKEN);
-      });
+    it("is no-op", () => {
+      const actions = store.getActions();
+      expect(actions).toHaveLength(0);
     });
   });
 
-  describe("persistToken", () => {
-    const enhancer = persistToken(cookies);
-    let store;
+  describe("with token", () => {
+    beforeEach(() => {
+      store = mockStore(initialState);
+      cookies.set("token", TOKEN);
+      loadToken(store, cookies);
+    });
 
-    describe("with cookie", () => {
-      beforeEach(() => {
-        cookies.set("token", TOKEN);
-        store = createStore(rootReducer, initialState, enhancer);
-      });
+    it("dispatches an action", () => {
+      const actions = store.getActions();
+      expect(actions).toHaveLength(1);
+    });
 
-      it("loads token", () => {
-        const state = store.getState();
-        expect(getToken(state)).toEqual(TOKEN);
+    it("dispatches FETCH_TOKEN_SUCCESS", () => {
+      const [action] = store.getActions();
+      expect(action.type).toEqual(FETCH_TOKEN_SUCCESS);
+    });
+
+    it("dispatches an action with loaded token", () => {
+      const [action] = store.getActions();
+      if (action.type === FETCH_TOKEN_SUCCESS) {
+        expect(action.token).toEqual(TOKEN);
+      }
+    });
+  });
+});
+
+describe("saveToken", () => {
+  let store;
+
+  beforeEach(() => {
+    cookies.remove("token");
+    store = createStore(rootReducer);
+    saveToken(store, cookies);
+  });
+
+  describe("initially", () => {
+    it("does not set cookie", () => {
+      const token = cookies.get("token");
+      expect(token).toBeFalsy();
+    });
+  });
+
+  describe("after unrelated action", () => {
+    beforeEach(() => {
+      store.dispatch({
+        type: "test"
       });
+    });
+
+    it("does not set cookie", () => {
+      const token = cookies.get("token");
+      expect(token).toBeFalsy();
+    });
+  });
+
+  describe("after token is fetched", () => {
+    beforeEach(() => {
+      store.dispatch(fetchTokenSuccess(TOKEN));
+    });
+
+    it("sets cookie", () => {
+      const token = cookies.get("token");
+      expect(token).toEqual(TOKEN);
+    });
+  });
+});
+
+describe("persistToken", () => {
+  const enhancer = persistToken(cookies);
+  let store;
+
+  describe("with cookie", () => {
+    beforeEach(() => {
+      cookies.set("token", TOKEN);
+      store = createStore(rootReducer, initialState, enhancer);
+    });
+
+    it("loads token", () => {
+      const state = store.getState();
+      expect(getToken(state)).toEqual(TOKEN);
     });
   });
 });
