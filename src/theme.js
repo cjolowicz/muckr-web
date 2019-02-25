fcae427c5639260bad4e996a3d66d69cf28ca255
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-25 10:40:47 +0100

    Switch primary palette color to blue

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,11 @@
 // @flow
 import { createMuiTheme } from "@material-ui/core/styles";
+import blue from "@material-ui/core/colors/blue";
 
 const theme = createMuiTheme({
+  palette: {
+    primary: blue
+  },
   typography: {
     useNextVariants: true
   }
