5a5d7d166c373d384d549557b612006ab1c107c5
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-25 13:43:01 +0100

    AuthButton: Add

--- src/containers/AppBar.js
+++ src/containers/AppBar.js
@@ -3,10 +3,8 @@ import { connect } from "react-redux";
 
 import AppBar from "../components/AppBar";
 import { openNavigation } from "../actions/navigation";
-import { clearToken } from "../actions/token";
-import { getToken } from "../reducers";
 
 export default connect(
-  state => ({ token: getToken(state) }),
-  { onMenuClick: openNavigation, clearToken }
+  null,
+  { onMenuClick: openNavigation }
 )(AppBar);
