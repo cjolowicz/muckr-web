29119f28be945ce4bf9f234fe3d36fe4ab08cdef
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-25 09:33:38 +0100

    Do not use AppBar layout on SignIn page
    Closes: #62

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 // @flow
-import React from "react";
+import * as React from "react";
 import { Route, Switch } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
@@ -11,21 +11,37 @@ import SignIn from "../containers/SignIn";
 import Message from "../containers/Message";
 import * as routes from "../routes";
 
-const Index = () => <h2>Welcome</h2>;
+const Layout = ({ children }: { children: React.Node }) => (
+  <>
+    <AppBar />
+    <Navigation />
+    {children}
+  </>
+);
+
+const Index = () => (
+  <Layout>
+    <h2>Welcome</h2>
+  </Layout>
+);
+
+const Artists = () => (
+  <Layout>
+    <FetchingArtistList />
+  </Layout>
+);
 
 const Routes = () => (
   <Switch>
     <Route path={routes.INDEX} exact component={Index} />
     <Route path={routes.SIGNIN} component={SignIn} />
-    <PrivateRoute path={routes.ARTISTS} component={FetchingArtistList} />
+    <PrivateRoute path={routes.ARTISTS} component={Artists} />
   </Switch>
 );
 
 const App = () => (
   <>
     <CssBaseline />
-    <AppBar />
-    <Navigation />
     <Routes />
     <Message />
   </>
