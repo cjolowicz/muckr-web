3550bd0d155510cf2ed7a42af278a062ee98be72
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-25 13:03:17 +0100

    Disable navigation actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,7 @@
 // @flow
 import type { MessageAction } from "./message";
-import type { NavigationAction } from "./navigation";
 import type { NoopAction } from "./noop";
 import type { TokenAction } from "./token";
 import type { ArtistAction } from "./artist";
 
-export type Action =
-  | NoopAction
-  | MessageAction
-  | NavigationAction
-  | TokenAction
-  | ArtistAction;
+export type Action = NoopAction | MessageAction | TokenAction | ArtistAction;
