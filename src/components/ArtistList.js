4b0838f458a217812535e2aa69ad6674c4d65754
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-25 11:31:03 +0100

    ArtistList: Remove redundant main and Paper

--- src/components/ArtistList.js
+++ src/components/ArtistList.js
@@ -3,7 +3,6 @@ import React from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 
 import type { Artist } from "../api/artist";
@@ -14,22 +13,20 @@ type Props = {
 };
 
 const ArtistList = ({ artists, isLoading }: Props) => (
-  <main>
-    <Paper>
-      {isLoading ? <Typography>Loading...</Typography> : null}
-      {!(isLoading || (artists && artists.length)) ? (
-        <Typography>No artists</Typography>
-      ) : null}
-      <List>
-        {artists &&
-          artists.map(artist => (
-            <ListItem key={artist.id} button>
-              <ListItemText primary={artist.name} />
-            </ListItem>
-          ))}
-      </List>
-    </Paper>
-  </main>
+  <>
+    {isLoading ? <Typography>Loading...</Typography> : null}
+    {!(isLoading || (artists && artists.length)) ? (
+      <Typography>No artists</Typography>
+    ) : null}
+    <List>
+      {artists &&
+        artists.map(artist => (
+          <ListItem key={artist.id} button>
+            <ListItemText primary={artist.name} />
+          </ListItem>
+        ))}
+    </List>
+  </>
 );
 
 export default ArtistList;
