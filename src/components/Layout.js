61ebb568df547d8401adf4920efcf078d4732f02
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-26 13:18:31 +0100

    Layout: Pad main

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,22 +1,30 @@
 // @flow
 import * as React from "react";
 import Paper from "@material-ui/core/Paper";
+import withStyles from "@material-ui/core/styles/withStyles";
 
 import AppBar from "./AppBar";
 import Navigation from "../containers/Navigation";
 
 type Props = {
-  children: React.Node
+  children: React.Node,
+  classes: Object
 };
 
-const Layout = ({ children }: Props) => (
+const Layout = ({ children, classes }: Props) => (
   <>
     <AppBar />
     <Navigation />
-    <main>
+    <main className={classes.main}>
       <Paper>{children}</Paper>
     </main>
   </>
 );
 
-export default Layout;
+const styles = theme => ({
+  main: {
+    padding: theme.spacing.unit * 3
+  }
+});
+
+export default withStyles(styles)(Layout);
