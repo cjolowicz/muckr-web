b8fff62f44eeb454b9f36c75b1947f3f305cb01b
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-27 23:03:05 +0100

    MenuButton: Toggle navigation state

--- src/containers/MenuButton.js
+++ src/containers/MenuButton.js
@@ -1,10 +1,22 @@
 // @flow
 import { connect } from "react-redux";
+import type { Dispatch } from "react-redux";
 
 import MenuButton from "../components/MenuButton";
-import { openNavigation } from "../actions/navigation";
+import { openNavigation, closeNavigation } from "../actions/navigation";
+import { isNavigationOpen } from "../reducers";
+
+type Props = {
+  navigationOpen: boolean
+};
+
+const mapDispatchToProps = (dispatch: Dispatch, { navigationOpen }: Props) => ({
+  onMenuClick() {
+    return dispatch(navigationOpen ? closeNavigation() : openNavigation());
+  }
+});
 
 export default connect(
-  null,
-  { onMenuClick: openNavigation }
+  state => ({ navigationOpen: isNavigationOpen(state) }),
+  mapDispatchToProps
 )(MenuButton);
