8e29e7efea976d6b7586db79df515b2b23fb20f9
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-03-05 19:20:14 +0100

    Add user actions

---
+++ src/actions/__tests__/user.js
@@ -0,0 +1,97 @@
+// @flow
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+
+import type { Dispatch } from "../user";
+import {
+  createUser,
+  CREATE_USER_REQUEST,
+  CREATE_USER_SUCCESS,
+  CREATE_USER_FAILURE
+} from "../user";
+import * as api from "../../api/user";
+import { unsafeCast } from "../../utils";
+import { mock } from "../../test/utils";
+import { USER } from "../../test/fixtures";
+
+const mockStore = configureStore([thunk]);
+
+const mockCreateUser = promise => {
+  beforeAll(() => {
+    jest.spyOn(api, "createUser").mockReturnValue(promise);
+  });
+
+  afterAll(() => {
+    mock(api.createUser).mockRestore();
+  });
+
+  return promise;
+};
+
+describe("createUser", () => {
+  describe("on success", () => {
+    const promise = mockCreateUser(Promise.resolve(USER));
+
+    it("dispatches CREATE_USER_REQUEST", async () => {
+      const store = mockStore({});
+      const dispatch = unsafeCast<Dispatch>(store.dispatch);
+
+      await dispatch(createUser("john", "secret"));
+
+      const actions = store.getActions();
+      const action = actions[0];
+
+      expect(action.type).toEqual(CREATE_USER_REQUEST);
+    });
+
+    it("dispatches CREATE_USER_SUCCESS", async () => {
+      const store = mockStore({});
+      const dispatch = unsafeCast<Dispatch>(store.dispatch);
+
+      await dispatch(createUser("john", "secret"));
+      await promise;
+
+      const actions = store.getActions();
+      const action = actions[1];
+
+      expect(action.type).toEqual(CREATE_USER_SUCCESS);
+    });
+  });
+
+  describe("on error", () => {
+    expect.assertions(1);
+
+    const error = Error("fail");
+    const promise = mockCreateUser(Promise.reject(error));
+
+    it("dispatches CREATE_USER_REQUEST", async () => {
+      const store = mockStore({});
+      const dispatch = unsafeCast<Dispatch>(store.dispatch);
+
+      await dispatch(createUser("john", "secret"));
+
+      const actions = store.getActions();
+      const action = actions[0];
+
+      expect(action.type).toEqual(CREATE_USER_REQUEST);
+    });
+
+    it("dispatches CREATE_USER_FAILURE", async () => {
+      const store = mockStore({});
+      const dispatch = unsafeCast<Dispatch>(store.dispatch);
+
+      await dispatch(createUser("john", "secret"));
+
+      try {
+        await promise;
+      } catch (unused) {
+        // ignore
+      }
+
+      const actions = store.getActions();
+      const action = actions[1];
+
+      expect(action.type).toEqual(CREATE_USER_FAILURE);
+    });
+  });
+});
---
+++ src/actions/user.js
@@ -0,0 +1,66 @@
+// @flow
+import * as api from "../api/user";
+import type { FetchError } from "../api/error";
+
+export const CREATE_USER_REQUEST = "CREATE_USER_REQUEST";
+export const CREATE_USER_SUCCESS = "CREATE_USER_SUCCESS";
+export const CREATE_USER_FAILURE = "CREATE_USER_FAILURE";
+
+export type CreateUserRequestAction = {
+  type: typeof CREATE_USER_REQUEST,
+  username: string,
+  password: string
+};
+
+export type CreateUserSuccessAction = {
+  type: typeof CREATE_USER_SUCCESS,
+  user: api.User
+};
+
+export type CreateUserFailureAction = {
+  type: typeof CREATE_USER_FAILURE,
+  error: FetchError
+};
+
+export type UserAction =
+  | CreateUserRequestAction
+  | CreateUserSuccessAction
+  | CreateUserFailureAction;
+
+export const createUserRequest = (
+  username: string,
+  password: string
+): CreateUserRequestAction => ({
+  type: CREATE_USER_REQUEST,
+  username,
+  password
+});
+
+export const createUserSuccess = (user: api.User): CreateUserSuccessAction => ({
+  type: CREATE_USER_SUCCESS,
+  user
+});
+
+export const createUserFailure = (
+  error: FetchError
+): CreateUserFailureAction => ({
+  type: CREATE_USER_FAILURE,
+  error
+});
+
+// eslint-disable-next-line no-use-before-define
+type ThunkAction = Dispatch => any;
+export type Dispatch = (UserAction | ThunkAction) => any;
+
+export const createUser = (username: string, password: string) => (
+  dispatch: Dispatch
+) => {
+  dispatch(createUserRequest(username, password));
+
+  return api
+    .createUser(username, password)
+    .then(
+      user => dispatch(createUserSuccess(user)),
+      error => dispatch(createUserFailure(error))
+    );
+};
