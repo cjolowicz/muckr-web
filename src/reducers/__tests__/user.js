d77bff9f906b549d9f224637e57083767cd3dd5d
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-03-05 20:07:05 +0100

    Add user reducers

---
+++ src/reducers/__tests__/user.js
@@ -0,0 +1,57 @@
+// @flow
+import user, { isCreatingUser, getUser, getUserError } from "../user";
+import { USER, GENERIC_ERROR } from "../../test/fixtures";
+import { noop } from "../../actions/noop";
+import {
+  createUserRequest,
+  createUserSuccess,
+  createUserFailure
+} from "../../actions/user";
+
+describe("user", () => {
+  describe("initially", () => {
+    const state = user(undefined, noop());
+
+    it("is not creating", () => {
+      expect(isCreatingUser(state)).toBe(false);
+    });
+
+    it("has no user", () => {
+      expect(getUser(state)).toBe(null);
+    });
+
+    it("has no error", () => {
+      expect(getUserError(state)).toBe(null);
+    });
+  });
+
+  describe("CREATE_USER_REQUEST", () => {
+    const state = user(undefined, createUserRequest("john", "secret"));
+
+    it("is creating", () => {
+      expect(isCreatingUser(state)).toBe(true);
+    });
+  });
+
+  describe("CREATE_USER_SUCCESS", () => {
+    const stateBefore = user(undefined, createUserRequest("john", "secret"));
+    const state = user(stateBefore, createUserSuccess(USER));
+
+    it("is not creating", () => {
+      expect(isCreatingUser(state)).toBe(false);
+    });
+
+    it("sets user", () => {
+      expect(getUser(state)).toEqual(USER);
+    });
+  });
+
+  describe("CREATE_USER_FAILURE", () => {
+    const stateBefore = user(undefined, createUserRequest("john", "secret"));
+    const state = user(stateBefore, createUserFailure(GENERIC_ERROR));
+
+    it("is not creating", () => {
+      expect(isCreatingUser(state)).toBe(false);
+    });
+  });
+});
---
+++ src/reducers/user.js
@@ -0,0 +1,66 @@
+// @flow
+import { combineReducers } from "redux";
+
+import {
+  CREATE_USER_REQUEST,
+  CREATE_USER_SUCCESS,
+  CREATE_USER_FAILURE
+} from "../actions/user";
+import type { Action } from "../actions";
+import type { FetchError } from "../api/error";
+
+export type State = {
+  isCreating: boolean,
+  user: ?string,
+  error: ?FetchError
+};
+
+export const initialState: State = {
+  isCreating: false,
+  user: null,
+  error: null
+};
+
+function isCreating(state = initialState.isCreating, action: Action) {
+  switch (action.type) {
+    case CREATE_USER_REQUEST:
+      return true;
+    case CREATE_USER_SUCCESS:
+      return false;
+    case CREATE_USER_FAILURE:
+      return false;
+    default:
+      return state;
+  }
+}
+
+function user(state = initialState.user, action: Action) {
+  switch (action.type) {
+    case CREATE_USER_SUCCESS:
+      return action.user;
+    case CREATE_USER_FAILURE:
+      return null;
+    default:
+      return state;
+  }
+}
+
+function error(state = initialState.error, action: Action) {
+  switch (action.type) {
+    case CREATE_USER_REQUEST:
+    case CREATE_USER_SUCCESS:
+      return null;
+    case CREATE_USER_FAILURE:
+      return action.error;
+    default:
+      return state;
+  }
+}
+
+export const isCreatingUser = (state: State) => state.isCreating;
+
+export const getUser = (state: State) => state.user;
+
+export const getUserError = (state: State) => state.error;
+
+export default combineReducers<Object, Action>({ isCreating, user, error });
