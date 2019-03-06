c18214fdd25fb705ac6fabc0cc7e1afc4d57c5ac
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-03-06 20:32:53 +0100

    AuthButton: Add SignUp button

--- src/components/AuthButton.js
+++ src/components/AuthButton.js
@@ -2,28 +2,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
+import withStyles from "@material-ui/core/styles/withStyles";
 
 import * as routes from "../routes";
 
 type Props = {
   token: ?string,
-  clearToken: Function
+  clearToken: Function,
+  classes: Object
 };
 
-const AuthButton = ({ token, clearToken }: Props) =>
+const AuthButton = ({ token, clearToken, classes }: Props) =>
   token ? (
     <Button onClick={clearToken} color="inherit">
       Sign Out
     </Button>
   ) : (
-    <Button
-      component={Link}
-      to={routes.SIGNIN}
-      color="inherit"
-      variant="outlined"
-    >
-      Sign In
-    </Button>
+    <>
+      <Button
+        component={Link}
+        to={routes.SIGNIN}
+        color="inherit"
+        variant="outlined"
+      >
+        Sign In
+      </Button>
+      <Button
+        component={Link}
+        to={routes.SIGNUP}
+        color="inherit"
+        variant="outlined"
+        className={classes.signUp}
+      >
+        Sign Up
+      </Button>
+    </>
   );
 
-export default AuthButton;
+const styles = theme => ({
+  signUp: {
+    marginLeft: theme.spacing.unit
+  }
+});
+
+export default withStyles(styles)(AuthButton);
