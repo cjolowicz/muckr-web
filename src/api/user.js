e9151d12cb7eb4b9f45e529561666286edc028a3
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-03-07 10:29:10 +0100

    api: Add getUsers

--- src/api/user.js
+++ src/api/user.js
@@ -2,19 +2,27 @@
 import axios from "axios";
 
 import { API_URL } from "../constants";
+import { createTokenAuthHeader } from "./token";
 
 export type User = {
   id: number,
   username: string
 };
 
-// eslint-disable-next-line import/prefer-default-export
 export async function createUser(username: string, password: string) {
   const email = `${username}@localhost`;
   const response = await axios.post(`${API_URL}/users`, {
     username,
     email,
     password
+  });
+
+  return response.data;
+}
+
+export async function getUsers(token: string) {
+  const response = await axios.get(`${API_URL}/users`, {
+    headers: createTokenAuthHeader(token)
   });
 
   return response.data;
