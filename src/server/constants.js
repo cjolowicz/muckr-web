8b2ece3302c4b3f07fc52d30a033fa834fbcead8
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-05-04 13:52:25 +0200

    Simplify server/constants

--- src/server/constants.js
+++ src/server/constants.js
@@ -3,13 +3,12 @@ import { fromMaybe } from "../utils";
 
 export const NODE_ENV = fromMaybe("development", process.env.NODE_ENV);
 export const PORT = fromMaybe("8000", process.env.PORT);
-export const PRODUCTION = NODE_ENV === "production";
 export const STATIC_PATH = "/static";
 export const WEBPACK_DIR = "dist";
 
 const WEBPACK_FILE = "js/bundle.js";
-const WEBPACK_PORT = 7000;
-const WEBPACK_URL = `http://localhost:${WEBPACK_PORT}/${WEBPACK_DIR}`;
+const WEBPACK_URL = "http://localhost:7000/dist";
 
-export const WEBPACK_PUBLIC_PATH = PRODUCTION ? STATIC_PATH : WEBPACK_URL;
+export const WEBPACK_PUBLIC_PATH =
+  NODE_ENV === "production" ? STATIC_PATH : WEBPACK_URL;
 export const WEBPACK_LOCATION = `${WEBPACK_PUBLIC_PATH}/${WEBPACK_FILE}`;
