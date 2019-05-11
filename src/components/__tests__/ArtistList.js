7744f62e43a6dd40fba37a360b3615474c70b3f5
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-05-11 17:30:23 +0200

    ArtistList: Use react-testing-library

--- src/components/__tests__/ArtistList.js
+++ src/components/__tests__/ArtistList.js
@@ -1,25 +1,29 @@
 // @flow
 import React from "react";
-import { mount } from "enzyme";
+import { render } from "react-testing-library";
 
 import ArtistList from "../ArtistList";
 import { ARTISTS } from "../../test/fixtures";
 
 describe("ArtistList", () => {
   describe("on success", () => {
-    it("renders artists", async () => {
-      const wrapper = mount(<ArtistList artists={ARTISTS} isLoading={false} />);
-      expect(wrapper.text()).toEqual(expect.stringContaining(ARTISTS[0].name));
+    it("renders artists", () => {
+      const { getByText } = render(
+        <ArtistList artists={ARTISTS} isLoading={false} />
+      );
+      expect(getByText(ARTISTS[0].name)).not.toBeNull();
     });
 
-    it("renders message while loading", async () => {
-      const wrapper = mount(<ArtistList artists={null} isLoading />);
-      expect(wrapper.find("p")).toHaveText("Loading...");
+    it("renders message while loading", () => {
+      const { getByText } = render(<ArtistList artists={ARTISTS} isLoading />);
+      expect(getByText("Loading...")).not.toBeNull();
     });
 
-    it("renders message if no artists", async () => {
-      const wrapper = mount(<ArtistList artists={null} isLoading={false} />);
-      expect(wrapper.find("p")).toHaveText("No artists");
+    it("renders message if no artists", () => {
+      const { getByText } = render(
+        <ArtistList artists={null} isLoading={false} />
+      );
+      expect(getByText("No artists")).not.toBeNull();
     });
   });
 });
