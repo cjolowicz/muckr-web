cc0a3e0458f1908232dcc281df84e6e57138699b
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-05-15 19:22:40 +0200

    [ArtistList] Add delete button to items

--- src/components/ArtistList.js
+++ src/components/ArtistList.js
@@ -1,8 +1,11 @@
 // @flow
 import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import Typography from "@material-ui/core/Typography";
 
 import type { Artist } from "../api/artist";
@@ -23,6 +26,11 @@ const ArtistList = ({ token, artists, isLoading, createArtist }: Props) => (
         artists.map(artist => (
           <ListItem key={artist.id} button>
             <ListItemText primary={artist.name} />
+            <ListItemSecondaryAction>
+              <IconButton aria-label="Delete" title="Delete">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
           </ListItem>
         ))}
     </List>
