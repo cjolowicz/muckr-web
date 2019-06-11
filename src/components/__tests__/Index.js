9f13888c6257526e673378fb392b4378a207041b
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-06-11 21:32:45 +0200

    [Index] Use testing-library

--- src/components/__tests__/Index.js
+++ src/components/__tests__/Index.js
@@ -3,10 +3,9 @@ import * as React from "react";
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
 import configureStore from "redux-mock-store";
-import { mount } from "enzyme";
+import { render } from "@testing-library/react";
 
 import Index from "../Index";
-import SignUp from "../../containers/SignUp";
 import rootReducer from "../../reducers";
 import { noop } from "../../actions/noop";
 import { TOKEN } from "../../test/fixtures";
@@ -18,14 +17,14 @@ describe("Index", () => {
 
   describe("with token", () => {
     it("does not render sign up form", () => {
-      const wrapper = mount(
+      const { queryByText } = render(
         <MemoryRouter initialEntries={["/"]}>
           <Provider store={store}>
             <Index token={TOKEN} />
           </Provider>
         </MemoryRouter>
       );
-      expect(wrapper.find(SignUp)).toHaveLength(0);
+      expect(queryByText("Sign up")).toBeNull();
     });
   });
 });
