63330e3945d5bda1a2588c78c872a14c5c5cb7d0
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-06-16 13:30:22 +0200

    [utils] Move just test to separate file

--- src/utils/__tests__/index.js
+++ src/utils/__tests__/index.js
@@ -1,29 +1,5 @@
 // @flow
-import { just, fromMaybe, unsafeCast } from "..";
-
-describe("just", () => {
-  test.each([[undefined], [null]])("throws if undefined or null", arg => {
-    expect(() => just(arg)).toThrow(Error);
-  });
-
-  test.each([
-    [false],
-    [true],
-    [0],
-    [1],
-    [2],
-    [""],
-    ["foo"],
-    [[]],
-    [["foo"]],
-    [{}],
-    [{ foo: "foo" }],
-    [() => null],
-    [() => undefined]
-  ])("returns argument", arg => {
-    expect(just(arg)).toBe(arg);
-  });
-});
+import { fromMaybe, unsafeCast } from "..";
 
 describe("fromMaybe", () => {
   test.each([[undefined], [null]])(
