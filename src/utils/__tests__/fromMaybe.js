7ca83e8f5346d28a86eb24f011c222d355c4622b
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-06-16 13:38:18 +0200

    [utils] Move fromMaybe test to a separate file

---
+++ src/utils/__tests__/fromMaybe.js
@@ -0,0 +1,29 @@
+// @flow
+import fromMaybe from "../fromMaybe";
+
+describe("fromMaybe", () => {
+  test.each([[undefined], [null]])(
+    "returns default if undefined or null",
+    arg => {
+      expect(fromMaybe("default", arg)).toBe("default");
+    }
+  );
+
+  test.each([
+    [false],
+    [true],
+    [0],
+    [1],
+    [2],
+    [""],
+    ["foo"],
+    [[]],
+    [["foo"]],
+    [{}],
+    [{ foo: "foo" }],
+    [() => null],
+    [() => undefined]
+  ])("returns value", arg => {
+    expect(fromMaybe("default", arg)).toBe(arg);
+  });
+});
--- src/utils/__tests__/index.js
+++ src/utils/__tests__/index.js
@@ -1,32 +1,5 @@
 // @flow
-import { fromMaybe, unsafeCast } from "..";
-
-describe("fromMaybe", () => {
-  test.each([[undefined], [null]])(
-    "returns default if undefined or null",
-    arg => {
-      expect(fromMaybe("default", arg)).toBe("default");
-    }
-  );
-
-  test.each([
-    [false],
-    [true],
-    [0],
-    [1],
-    [2],
-    [""],
-    ["foo"],
-    [[]],
-    [["foo"]],
-    [{}],
-    [{ foo: "foo" }],
-    [() => null],
-    [() => undefined]
-  ])("returns value", arg => {
-    expect(fromMaybe("default", arg)).toBe(arg);
-  });
-});
+import { unsafeCast } from "..";
 
 describe("unsafeCast", () => {
   test.each([[1, "foo", null]])("returns value", arg => {
