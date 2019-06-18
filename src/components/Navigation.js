5a50eb45426211b23764401f09ce0a71a1b0ed96
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-06-18 23:42:45 +0200

    [Navigation] Use styles hook instead of HOC

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,38 +1,40 @@
 // @flow
 import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/styles";
 
 import * as routes from "../routes";
 
 type Props = {
   open: boolean,
-  classes: Object,
   onClose: Function
 };
 
-const Navigation = ({ open, classes, onClose }: Props) => (
-  <Drawer open={open} onClose={onClose}>
-    <div tabIndex={0} role="button" onClick={onClose} onKeyDown={onClose}>
-      <div className={classes.list}>
-        <List>
-          <ListItem button component={Link} to={routes.ARTISTS}>
-            <ListItemText primary="Artists" />
-          </ListItem>
-        </List>
-      </div>
-    </div>
-  </Drawer>
-);
-
-const styles = {
+const useStyles = makeStyles({
   list: {
     width: 250
   }
+});
+
+const Navigation = ({ open, onClose }: Props) => {
+  const classes = useStyles();
+  return (
+    <Drawer open={open} onClose={onClose}>
+      <div tabIndex={0} role="button" onClick={onClose} onKeyDown={onClose}>
+        <div className={classes.list}>
+          <List>
+            <ListItem button component={Link} to={routes.ARTISTS}>
+              <ListItemText primary="Artists" />
+            </ListItem>
+          </List>
+        </div>
+      </div>
+    </Drawer>
+  );
 };
 
-export default withStyles(styles)(Navigation);
+export default Navigation;
