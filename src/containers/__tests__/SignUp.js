3326bd12a7b55418920422aad263b52efa1c21fc
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-06-24 13:50:37 +0200

    [SignUp] Use render helper in container test

--- src/containers/__tests__/SignUp.js
+++ src/containers/__tests__/SignUp.js
@@ -1,33 +1,12 @@
 // @flow
 import React from "react";
-import { Provider } from "react-redux";
-import { MemoryRouter } from "react-router-dom";
-import configureStore from "redux-mock-store";
-import thunk from "redux-thunk";
-import { ThemeProvider } from "@material-ui/styles";
-import { render } from "@testing-library/react";
 
 import SignUp from "../SignUp";
-import rootReducer from "../../reducers";
-import { noop } from "../../actions/noop";
-import theme from "../../theme";
+import render from "../../test/render";
 
 describe("SignUp", () => {
   it("renders", () => {
-    const mockStore = configureStore([thunk]);
-    const state = rootReducer(undefined, noop());
-    const store = mockStore(state);
-    const routerEntry = { pathname: "/join", key: "test" };
-    const { queryByText } = render(
-      <MemoryRouter initialEntries={[routerEntry]}>
-        <ThemeProvider theme={theme}>
-          <Provider store={store}>
-            <SignUp />
-          </Provider>
-        </ThemeProvider>
-      </MemoryRouter>
-    );
-
+    const { queryByText } = render(<SignUp />, { route: "/join" });
     expect(queryByText("Sign up for Muckr")).not.toBeNull();
   });
 });
