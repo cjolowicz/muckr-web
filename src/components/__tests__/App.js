295efb31c5ef49cb432c8d39374153671e780797
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-06-24 11:48:21 +0200

    [App] Use render helper in test

--- src/components/__tests__/App.js
+++ src/components/__tests__/App.js
@@ -1,60 +1,33 @@
 // @flow
 import React from "react";
-import { Provider } from "react-redux";
-import { render } from "@testing-library/react";
-import { ThemeProvider } from "@material-ui/styles";
-import { MemoryRouter } from "react-router-dom";
-import configureStore from "redux-mock-store";
-import thunk from "redux-thunk";
 
 import App from "../App";
-import rootReducer from "../../reducers";
-import { noop } from "../../actions/noop";
+import render from "../../test/render";
 import { TOKEN, ARTIST, ARTISTS } from "../../test/fixtures";
 import * as routes from "../../routes";
 import { fetchTokenSuccess } from "../../actions/token";
 import { fetchArtistsSuccess } from "../../actions/artist";
-import theme from "../../theme";
-
-const renderAppWithRoute = (route, actions = []) => {
-  const state = (actions.length ? actions : [noop()]).reduce(
-    rootReducer,
-    undefined
-  );
-  const mockStore = configureStore([thunk]);
-  const store = mockStore(state);
-  const routerEntry = { pathname: route, key: "test" };
-
-  return render(
-    <MemoryRouter initialEntries={[routerEntry]}>
-      <ThemeProvider theme={theme}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </ThemeProvider>
-    </MemoryRouter>
-  );
-};
 
 describe("App", () => {
   it("renders welcome page", () => {
-    const { queryByText } = renderAppWithRoute(routes.INDEX, [
-      fetchTokenSuccess(TOKEN)
-    ]);
+    const { queryByText } = render(<App />, {
+      route: routes.INDEX,
+      actions: [fetchTokenSuccess(TOKEN)]
+    });
     expect(queryByText("Welcome")).not.toBeNull();
   });
 
   it("renders sign in box", () => {
-    const { queryByText } = renderAppWithRoute(routes.SIGNIN);
+    const { queryByText } = render(<App />, { route: routes.SIGNIN });
     expect(queryByText("Sign in to Muckr")).not.toBeNull();
   });
 
   it("renders artist list", () => {
     const { name } = ARTIST;
-    const { queryByText } = renderAppWithRoute(routes.ARTISTS, [
-      fetchTokenSuccess(TOKEN),
-      fetchArtistsSuccess(ARTISTS)
-    ]);
+    const { queryByText } = render(<App />, {
+      route: routes.ARTISTS,
+      actions: [fetchTokenSuccess(TOKEN), fetchArtistsSuccess(ARTISTS)]
+    });
     expect(queryByText(name)).not.toBeNull();
   });
 });
