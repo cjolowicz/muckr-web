8ba1550368fd447c3c0a36e33d4aef1ad67a1c29
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-06-24 14:09:48 +0200

    [SignUp] Use specialized render helper in redirect test

--- src/components/__tests__/SignUp.js
+++ src/components/__tests__/SignUp.js
@@ -12,10 +12,19 @@ const onSubmit = jest.fn();
 
 afterEach(() => onSubmit.mockClear());
 
-const renderSignUp = () => {
-  const { container } = render(<SignUp onSubmit={onSubmit} user={null} />);
+const renderSignUp = ({ user }) => {
+  const utils = render(
+    <Switch>
+      <Route path="/join">
+        <SignUp onSubmit={onSubmit} user={user} />
+      </Route>
+    </Switch>,
+    { route: "/join" }
+  );
 
+  const { container } = utils;
   return {
+    ...utils,
     header: select<HTMLHeadingElement>(container, "h1"),
     username: select<HTMLInputElement>(container, "input[name='username']"),
     password: select<HTMLInputElement>(container, "input[name='password']"),
@@ -26,14 +35,18 @@ const renderSignUp = () => {
 describe("SignUp", () => {
   describe("initially", () => {
     it("renders title", () => {
-      const { header } = renderSignUp();
+      const { header } = renderSignUp({ user: null });
       expect(header && header.textContent).toBe("Sign up for Muckr");
     });
   });
 
   describe("handleUsernameChange", () => {
     it("updates state", () => {
-      const { username } = renderSignUp();
+      const { username } = renderSignUp({ user: null });
+
+      expect(username).not.toBeNull();
+      if (!username) return;
+
       fireEvent.change(username, { target: { value: "john" } });
       expect(username && username.value).toEqual("john");
     });
@@ -41,7 +54,11 @@ describe("SignUp", () => {
 
   describe("handlePasswordChange", () => {
     it("updates state", () => {
-      const { password } = renderSignUp();
+      const { password } = renderSignUp({ user: null });
+
+      expect(password).not.toBeNull();
+      if (!password) return;
+
       fireEvent.change(password, { target: { value: "secret" } });
       expect(password && password.value).toEqual("secret");
     });
@@ -49,7 +66,12 @@ describe("SignUp", () => {
 
   describe("handleSubmit", () => {
     it("invokes onSubmit", async () => {
-      const { username, password, submit } = renderSignUp();
+      const { username, password, submit } = renderSignUp({ user: null });
+
+      expect(username).not.toBeNull();
+      expect(password).not.toBeNull();
+      expect(submit).not.toBeNull();
+      if (!(username && password && submit)) return;
 
       fireEvent.change(username, { target: { value: "john" } });
       fireEvent.change(password, { target: { value: "secret" } });
@@ -61,15 +83,7 @@ describe("SignUp", () => {
 
   describe("on success", () => {
     it("redirects to /login", () => {
-      const { history } = render(
-        <Switch>
-          <Route path="/join">
-            <SignUp onSubmit={onSubmit} user={USER} />
-          </Route>
-        </Switch>,
-        { route: "/join" }
-      );
-
+      const { history } = renderSignUp({ user: USER });
       expect(history.location.pathname).toEqual("/login");
     });
   });
