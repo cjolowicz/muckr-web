a250c589919585385aa85ae4b16a7489b34a44f0
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-06-24 12:39:07 +0200

    [ArtistList] Simplify test using specialized render helper

--- src/components/__tests__/ArtistList.js
+++ src/components/__tests__/ArtistList.js
@@ -6,52 +6,46 @@ import ArtistList from "../ArtistList";
 import render from "../../test/render";
 import { ARTISTS, ARTIST, TOKEN } from "../../test/fixtures";
 
+const renderArtistList = ({ artists, isLoading }) => {
+  const openDialog = jest.fn();
+  const utils = render(
+    <ArtistList
+      artists={artists}
+      createArtist={jest.fn()}
+      removeArtist={jest.fn()}
+      openDialog={openDialog}
+      isLoading={isLoading}
+      token={TOKEN}
+    />
+  );
+  return { ...utils, openDialog };
+};
+
 describe("ArtistList", () => {
   describe("on success", () => {
     it("renders artists", () => {
-      const createArtist = jest.fn();
-      const { getByText } = render(
-        <ArtistList
-          artists={ARTISTS}
-          createArtist={createArtist}
-          removeArtist={jest.fn()}
-          openDialog={jest.fn()}
-          isLoading={false}
-          token={TOKEN}
-        />
-      );
+      const { getByText } = renderArtistList({
+        artists: ARTISTS,
+        isLoading: false
+      });
       expect(getByText(ARTISTS[0].name)).not.toBeNull();
     });
 
     it("renders message while loading", () => {
-      const createArtist = jest.fn();
-      const { getByText } = render(
-        <ArtistList
-          artists={ARTISTS}
-          createArtist={createArtist}
-          removeArtist={jest.fn()}
-          openDialog={jest.fn()}
-          isLoading
-          token={TOKEN}
-        />
-      );
+      const { getByText } = renderArtistList({
+        artists: ARTISTS,
+        isLoading: true
+      });
       expect(getByText("Loading...")).not.toBeNull();
     });
   });
 
   describe("on edit", () => {
     it("opens dialog", () => {
-      const openDialog = jest.fn();
-      const { getByTitle } = render(
-        <ArtistList
-          artists={[ARTIST]}
-          createArtist={jest.fn()}
-          removeArtist={jest.fn()}
-          isLoading={false}
-          token={TOKEN}
-          openDialog={openDialog}
-        />
-      );
+      const { getByTitle, openDialog } = renderArtistList({
+        artists: [ARTIST],
+        isLoading: false
+      });
 
       const editButton = getByTitle("Edit");
       fireEvent.click(editButton);
