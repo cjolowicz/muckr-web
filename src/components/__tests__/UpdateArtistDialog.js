906b1746ed5b6d8fc66b83d31513fbc8c0df475c
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-06-24 13:26:49 +0200

    [UpdateArtistDialog] Simplify test using specialized render helper

--- src/components/__tests__/UpdateArtistDialog.js
+++ src/components/__tests__/UpdateArtistDialog.js
@@ -5,20 +5,29 @@ import { render, fireEvent } from "@testing-library/react";
 import UpdateArtistDialog from "../UpdateArtistDialog";
 import { ARTIST, TOKEN } from "../../test/fixtures";
 
+const renderUpdateArtistDialog = ({ token }) => {
+  const closeDialog = jest.fn();
+  const updateArtist = jest.fn();
+  const updateDialog = jest.fn();
+  const utils = render(
+    <UpdateArtistDialog
+      open
+      updateDialog={updateDialog}
+      closeDialog={closeDialog}
+      updateArtist={updateArtist}
+      token={token}
+      artist={ARTIST}
+    />
+  );
+  return { ...utils, closeDialog, updateArtist, updateDialog };
+};
+
 describe("UpdateArtistDialog", () => {
   describe("on change", () => {
     it("triggers updateDialog", () => {
-      const updateDialog = jest.fn();
-      const { getByLabelText } = render(
-        <UpdateArtistDialog
-          open
-          updateDialog={updateDialog}
-          closeDialog={jest.fn()}
-          updateArtist={jest.fn()}
-          token={TOKEN}
-          artist={ARTIST}
-        />
-      );
+      const { getByLabelText, updateDialog } = renderUpdateArtistDialog({
+        token: TOKEN
+      });
       const textField = getByLabelText("Name");
       fireEvent.change(textField, { target: { value: "x" } });
       expect(updateDialog).toHaveBeenCalledWith("x");
@@ -27,34 +36,18 @@ describe("UpdateArtistDialog", () => {
 
   describe("on submit", () => {
     it("updates artist", () => {
-      const updateArtist = jest.fn();
-      const { getByText } = render(
-        <UpdateArtistDialog
-          open
-          updateDialog={jest.fn()}
-          closeDialog={jest.fn()}
-          updateArtist={updateArtist}
-          token={TOKEN}
-          artist={ARTIST}
-        />
-      );
+      const { getByText, updateArtist } = renderUpdateArtistDialog({
+        token: TOKEN
+      });
       const submitButton = getByText("Update");
       fireEvent.click(submitButton);
       expect(updateArtist).toHaveBeenCalledWith(TOKEN, ARTIST);
     });
 
     it("closes dialog", () => {
-      const closeDialog = jest.fn();
-      const { getByText } = render(
-        <UpdateArtistDialog
-          open
-          updateDialog={jest.fn()}
-          closeDialog={closeDialog}
-          updateArtist={jest.fn()}
-          token={null}
-          artist={null}
-        />
-      );
+      const { getByText, closeDialog } = renderUpdateArtistDialog({
+        token: null
+      });
       const submitButton = getByText("Update");
       fireEvent.click(submitButton);
       expect(closeDialog).toHaveBeenCalled();
