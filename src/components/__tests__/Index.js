6cc0c356cdf2eb7dbb7986702382ab69b117603b
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-06-24 11:26:18 +0200

    [Index] Use render helper in test

--- src/components/__tests__/Index.js
+++ src/components/__tests__/Index.js
@@ -1,33 +1,14 @@
 // @flow
 import * as React from "react";
-import { Provider } from "react-redux";
-import { ThemeProvider } from "@material-ui/styles";
-import { MemoryRouter } from "react-router-dom";
-import configureStore from "redux-mock-store";
-import { render } from "@testing-library/react";
 
 import Index from "../Index";
-import rootReducer from "../../reducers";
-import { noop } from "../../actions/noop";
+import render from "../../test/render";
 import { TOKEN } from "../../test/fixtures";
-import theme from "../../theme";
 
 describe("Index", () => {
-  const mockStore = configureStore([]);
-  const state = rootReducer(undefined, noop());
-  const store = mockStore(state);
-
   describe("with token", () => {
     it("does not render sign up form", () => {
-      const { queryByText } = render(
-        <MemoryRouter initialEntries={["/"]}>
-          <ThemeProvider theme={theme}>
-            <Provider store={store}>
-              <Index token={TOKEN} />
-            </Provider>
-          </ThemeProvider>
-        </MemoryRouter>
-      );
+      const { queryByText } = render(<Index token={TOKEN} />);
       expect(queryByText("Sign up")).toBeNull();
     });
   });
