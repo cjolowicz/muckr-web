f3991bfb07ff2f2979c2e943c0d90b3b54f5c547
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-06-24 11:26:04 +0200

    [test] Add render wrapper function

---
+++ src/test/render.js
@@ -0,0 +1,46 @@
+// @flow
+import * as React from "react";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import { ThemeProvider } from "@material-ui/styles";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import { render } from "@testing-library/react";
+
+import { noop } from "../actions/noop";
+import rootReducer from "../reducers";
+import theme from "../theme";
+import type { Action } from "../actions";
+
+type RenderConfig = {
+  actions?: Array<Action>,
+  route?: string
+};
+
+const render$ = (
+  component: React.Element<*>,
+  { actions, route }: RenderConfig = {}
+) => {
+  const actions$ = actions && actions.length ? actions : [noop()];
+
+  const state = actions$.reduce(rootReducer, undefined);
+  const mockStore = configureStore([thunk]);
+  const store = mockStore(state);
+
+  const history = createMemoryHistory({
+    initialEntries: [{ pathname: route || "/", key: "test" }]
+  });
+
+  const utils = render(
+    <Router history={history}>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>{component}</Provider>
+      </ThemeProvider>
+    </Router>
+  );
+
+  return { ...utils, history };
+};
+
+export default render$;
