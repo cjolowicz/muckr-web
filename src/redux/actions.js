d23837f93df6e6a6773a55a8f19468db275a0fb9
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-06 16:21:04 +0200

    [actions] Eliminate redundant directory

---
+++ src/redux/actions.js
@@ -0,0 +1,15 @@
+// @flow
+import type { DialogAction } from "./dialog/actions";
+import type { MessageAction } from "./message/actions";
+import type { NavigationAction } from "./navigation/actions";
+import type { NoopAction } from "./noop/actions";
+import type { TokenAction } from "./token/actions";
+import type { ArtistAction } from "./artist/actions";
+
+export type Action =
+  | NoopAction
+  | DialogAction
+  | MessageAction
+  | NavigationAction
+  | TokenAction
+  | ArtistAction;
