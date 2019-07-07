8f279df27b2045b603b5cefe175e8a4ec0fe9d2f
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-07 12:15:33 +0200

    [dialog] Minor cleanup in selectors

--- src/redux/dialog/selectors.js
+++ src/redux/dialog/selectors.js
@@ -1,13 +1,13 @@
 // @flow
 import * as constants from "./constants";
-import * as types from "./types";
+import type { State } from "./types";
 
-export const isCreateDialogOpen = (state: types.State) =>
+export const isCreateDialogOpen = (state: State) =>
   state.open === constants.DIALOG_TYPE_CREATE;
 
-export const isUpdateDialogOpen = (state: types.State) =>
+export const isUpdateDialogOpen = (state: State) =>
   state.open === constants.DIALOG_TYPE_UPDATE;
 
-export const getDialogType = (state: types.State) => state.open;
+export const getDialogType = (state: State) => state.open;
 
-export const getDialogArtist = (state: types.State) => state.artist;
+export const getDialogArtist = (state: State) => state.artist;
