d9cb0e2ddf8841e84bd6fd7d7f00ff34459b79e7
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-07 12:53:05 +0200

    [SignUp] Use createStructuredSelector

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -1,11 +1,12 @@
 // @flow
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import SignUp from "../components/SignUp";
 import { createUser } from "../redux/user/operations";
 import { getUser } from "../redux/selectors";
 
 export default connect(
-  state => ({ user: getUser(state) }),
+  createStructuredSelector({ user: getUser }),
   { onSubmit: createUser }
 )(SignUp);
