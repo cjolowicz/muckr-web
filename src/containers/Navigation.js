ab85e0da40106c4fd975d4bff632fcd59d8e0811
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-07 12:52:42 +0200

    [Navigation] Use createStructuredSelector

--- src/containers/Navigation.js
+++ src/containers/Navigation.js
@@ -1,11 +1,12 @@
 // @flow
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import Navigation from "../components/Navigation";
 import { closeNavigation } from "../redux/navigation/actions";
 import { isNavigationOpen } from "../redux/selectors";
 
 export default connect(
-  state => ({ open: isNavigationOpen(state) }),
+  createStructuredSelector({ open: isNavigationOpen }),
   { onClose: closeNavigation }
 )(Navigation);
