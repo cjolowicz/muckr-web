f572391d2862d1339d0aa9613c5b497617533da1
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-07 12:18:12 +0200

    [redux] Minor cleanup in selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,55 +1,51 @@
 // @flow
-import * as dialogSelectors from "./dialog/selectors";
-import * as messageSelectors from "./message/selectors";
-import * as navigationSelectors from "./navigation/selectors";
-import * as tokenSelectors from "./token/selectors";
-import * as userSelectors from "./user/selectors";
-import * as artistSelectors from "./artist/selectors";
+import * as dialog from "./dialog/selectors";
+import * as message from "./message/selectors";
+import * as navigation from "./navigation/selectors";
+import * as token from "./token/selectors";
+import * as user from "./user/selectors";
+import * as artist from "./artist/selectors";
 import { getItemsById } from "../utils";
 import type { State } from "./types";
 
 export const isCreateDialogOpen = (state: State) =>
-  dialogSelectors.isCreateDialogOpen(state.dialog);
+  dialog.isCreateDialogOpen(state.dialog);
 
 export const isUpdateDialogOpen = (state: State) =>
-  dialogSelectors.isUpdateDialogOpen(state.dialog);
+  dialog.isUpdateDialogOpen(state.dialog);
 
 export const getDialogArtist = (state: State) =>
-  dialogSelectors.getDialogArtist(state.dialog);
+  dialog.getDialogArtist(state.dialog);
 
 export const getDialogType = (state: State) =>
-  dialogSelectors.getDialogType(state.dialog);
+  dialog.getDialogType(state.dialog);
 
 export const isMessageOpen = (state: State) =>
-  messageSelectors.isMessageOpen(state.message);
+  message.isMessageOpen(state.message);
 
-export const getMessage = (state: State) =>
-  messageSelectors.getMessage(state.message);
+export const getMessage = (state: State) => message.getMessage(state.message);
 
 export const isNavigationOpen = (state: State) =>
-  navigationSelectors.isNavigationOpen(state.navigation);
+  navigation.isNavigationOpen(state.navigation);
 
 export const isFetchingToken = (state: State) =>
-  tokenSelectors.isFetchingToken(state.token);
+  token.isFetchingToken(state.token);
 
-export const getToken = (state: State) => tokenSelectors.getToken(state.token);
+export const getToken = (state: State) => token.getToken(state.token);
 
-export const getTokenError = (state: State) =>
-  tokenSelectors.getTokenError(state.token);
+export const getTokenError = (state: State) => token.getTokenError(state.token);
 
-export const isCreatingUser = (state: State) =>
-  userSelectors.isCreatingUser(state.user);
+export const isCreatingUser = (state: State) => user.isCreatingUser(state.user);
 
-export const getUser = (state: State) => userSelectors.getUser(state.user);
+export const getUser = (state: State) => user.getUser(state.user);
 
-export const getUserError = (state: State) =>
-  userSelectors.getUserError(state.user);
+export const getUserError = (state: State) => user.getUserError(state.user);
 
 export const isFetchingArtists = (state: State) =>
-  artistSelectors.isFetchingArtists(state.artists);
+  artist.isFetchingArtists(state.artists);
 
 export const getArtists = (state: State) =>
   getItemsById(state.entities.artists, state.artists.ids);
 
 export const getArtistsError = (state: State) =>
-  artistSelectors.getArtistsError(state.artists);
+  artist.getArtistsError(state.artists);
