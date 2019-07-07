df68a2123dbaa684609da35b8ffc65a8e074319b
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-07 12:52:58 +0200

    [SignIn] Use createStructuredSelector

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -1,11 +1,12 @@
 // @flow
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import SignIn from "../components/SignIn";
 import { fetchToken } from "../redux/token/operations";
 import { getToken } from "../redux/selectors";
 
 export default connect(
-  state => ({ token: getToken(state) }),
+  createStructuredSelector({ token: getToken }),
   { onSubmit: fetchToken }
 )(SignIn);
