5d3ded3c1cf489ff3a6ed52afa091abe3af4ee61
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-07 12:51:57 +0200

    [AuthButton] Use createStructuredSelector

--- src/containers/AuthButton.js
+++ src/containers/AuthButton.js
@@ -1,11 +1,12 @@
 // @flow
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import AuthButton from "../components/AuthButton";
 import { clearToken } from "../redux/token/actions";
 import { getToken } from "../redux/selectors";
 
 export default connect(
-  state => ({ token: getToken(state) }),
+  createStructuredSelector({ token: getToken }),
   { clearToken }
 )(AuthButton);
