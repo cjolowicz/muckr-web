8a860b422d1055c69aaa847d6f82413dab18bb66
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-10 18:59:14 +0200

    [user] Remove selector `pending`

--- src/redux/user/__tests__/reducers.js
+++ src/redux/user/__tests__/reducers.js
@@ -1,6 +1,6 @@
 // @flow
 import reducer from "../reducers";
-import { pending, user, error } from "../selectors";
+import { user, error } from "../selectors";
 import { USER, GENERIC_ERROR } from "../../../test/fixtures";
 import { noop } from "../../noop/actions";
 import {
@@ -12,10 +12,6 @@ import {
 describe("user", () => {
   describe("initially", () => {
     const state = reducer(undefined, noop());
-
-    it("is not creating", () => {
-      expect(pending(state)).toBe(false);
-    });
 
     it("has no user", () => {
       expect(user(state)).toBe(null);
@@ -29,18 +25,14 @@ describe("user", () => {
   describe("CREATE_USER_REQUEST", () => {
     const state = reducer(undefined, createUserRequest("john", "secret"));
 
-    it("is creating", () => {
-      expect(pending(state)).toBe(true);
+    it("has no user", () => {
+      expect(user(state)).toBe(null);
     });
   });
 
   describe("CREATE_USER_SUCCESS", () => {
     const stateBefore = reducer(undefined, createUserRequest("john", "secret"));
     const state = reducer(stateBefore, createUserSuccess(USER));
-
-    it("is not creating", () => {
-      expect(pending(state)).toBe(false);
-    });
 
     it("sets user", () => {
       expect(user(state)).toEqual(USER);
@@ -51,8 +43,8 @@ describe("user", () => {
     const stateBefore = reducer(undefined, createUserRequest("john", "secret"));
     const state = reducer(stateBefore, createUserFailure(GENERIC_ERROR));
 
-    it("is not creating", () => {
-      expect(pending(state)).toBe(false);
+    it("has no user", () => {
+      expect(user(state)).toBe(null);
     });
   });
 });
--- src/redux/user/selectors.js
+++ src/redux/user/selectors.js
@@ -1,8 +1,6 @@
 // @flow
 import type { State } from "./types";
 
-export const pending = (state: State) => state.pending;
-
 export const user = (state: State) => state.user;
 
 export const error = (state: State) => state.error;
