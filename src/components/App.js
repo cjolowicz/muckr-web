efb909bfb803c0068c70db18be80d2b76292afd7
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-11 20:03:26 +0200

    [ArtistsPage] Add

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
-import Layout from "./Layout";
+import ArtistsPage from "./pages/ArtistsPage";
 import PrivateRoute from "../containers/PrivateRoute";
-import FetchingArtistList from "../containers/FetchingArtistList";
 import Index from "../containers/Index";
 import SignIn from "../containers/SignIn";
 import Message from "../containers/Message";
 import * as routes from "../routes";
-
-const Artists = () => (
-  <Layout>
-    <FetchingArtistList />
-  </Layout>
-);
 
 const App = () => (
   <>
@@ -23,7 +16,7 @@ const App = () => (
     <Switch>
       <Route path={routes.INDEX} exact component={Index} />
       <Route path={routes.SIGNIN} component={SignIn} />
-      <PrivateRoute path={routes.ARTISTS} component={Artists} />
+      <PrivateRoute path={routes.ARTISTS} component={ArtistsPage} />
     </Switch>
     <Message />
   </>
---
+++ src/components/pages/ArtistsPage.js
@@ -0,0 +1,13 @@
+// @flow
+import React from "react";
+
+import Layout from "../Layout";
+import FetchingArtistList from "../../containers/FetchingArtistList";
+
+const ArtistsPage = () => (
+  <Layout>
+    <FetchingArtistList />
+  </Layout>
+);
+
+export default ArtistsPage;
