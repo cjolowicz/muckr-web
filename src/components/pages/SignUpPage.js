1d20f45002e2f3ceb87e60cd7444162b1e20a414
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-12 20:53:24 +0200

    [SignUpPage] Add

---
+++ src/components/pages/SignUpPage.js
@@ -0,0 +1,13 @@
+// @flow
+import React from "react";
+
+import SignUpLayout from "../layouts/SignUpLayout";
+import SignUp from "../SignUp";
+
+export const SignUpPage = () => (
+  <SignUpLayout>
+    <SignUp />
+  </SignUpLayout>
+);
+
+export default SignUpPage;
