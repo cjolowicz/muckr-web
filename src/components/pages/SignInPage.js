2e3682aab3fc10ce3e94b07401d0b3fc6aaf925e
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-12 19:02:01 +0200

    [SignInPage] Add

---
+++ src/components/pages/SignInPage.js
@@ -0,0 +1,13 @@
+// @flow
+import React from "react";
+
+import SignInLayout from "../layouts/SignInLayout";
+import SignIn from "./SignIn";
+
+const SignInPage = () => (
+  <SignInLayout>
+    <SignIn />
+  </SignInLayout>
+);
+
+export default SignInPage;
