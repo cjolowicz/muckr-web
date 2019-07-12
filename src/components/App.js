ec73e505e982697ed505e2385b30af8341f56b83
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-12 12:00:43 +0200

    [SignIn] Move to pages directory (container)

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import ArtistsPage from "./pages/ArtistsPage";
 import PrivateRoute from "../containers/PrivateRoute";
 import Index from "../containers/pages/Index";
-import SignIn from "../containers/SignIn";
+import SignIn from "../containers/pages/SignIn";
 import Message from "../containers/Message";
 import * as routes from "../routes";
 
--- src/containers/pages/SignIn.js
+++ src/containers/pages/SignIn.js
@@ -2,9 +2,9 @@
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import SignIn from "../components/pages/SignIn";
-import { fetchToken } from "../redux/token/operations";
-import { token } from "../redux/selectors";
+import SignIn from "../../components/pages/SignIn";
+import { fetchToken } from "../../redux/token/operations";
+import { token } from "../../redux/selectors";
 
 export default connect(
   createStructuredSelector({ token }),
