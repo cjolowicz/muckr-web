58d76f94fe6446d8e2c3097b25cd970e9b192f5f
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-12 11:33:33 +0200

    [Index] Move to pages directory (container)

--- src/containers/pages/Index.js
+++ src/containers/pages/Index.js
@@ -2,8 +2,8 @@
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import Index from "../components/pages/Index";
-import { token } from "../redux/selectors";
+import Index from "../../components/pages/Index";
+import { token } from "../../redux/selectors";
 
 export default connect(
   createStructuredSelector({ token }),
