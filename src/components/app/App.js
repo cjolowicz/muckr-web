90243e3c00f4cf999793cbe19986b00cb2b63254
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-12 21:17:36 +0200

    [App] Use Routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,13 @@
 // @flow
 import React from "react";
-import { Route, Switch } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
-import ArtistsPage from "../pages/ArtistsPage";
-import PrivateRoute from "../PrivateRoute";
-import FrontPage from "../pages/FrontPage";
-import SignInPage from "../pages/SignInPage";
-import * as routes from "../../routes";
+import Routes from "./Routes";
 
 const App = () => (
   <>
     <CssBaseline />
-    <Switch>
-      <Route path={routes.INDEX} exact component={FrontPage} />
-      <Route path={routes.SIGNIN} component={SignInPage} />
-      <PrivateRoute path={routes.ARTISTS} component={ArtistsPage} />
-    </Switch>
+    <Routes />
   </>
 );
 
