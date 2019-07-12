918d65d988a80ececf44e0195d4e4b3aedac5800
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-12 21:17:27 +0200

    [Routes] Add

---
+++ src/components/app/Routes.js
@@ -0,0 +1,19 @@
+// @flow
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import ArtistsPage from "../pages/ArtistsPage";
+import PrivateRoute from "../PrivateRoute";
+import FrontPage from "../pages/FrontPage";
+import SignInPage from "../pages/SignInPage";
+import * as routes from "../../routes";
+
+const Routes = () => (
+  <Switch>
+    <Route path={routes.INDEX} exact component={FrontPage} />
+    <Route path={routes.SIGNIN} component={SignInPage} />
+    <PrivateRoute path={routes.ARTISTS} component={ArtistsPage} />
+  </Switch>
+);
+
+export default Routes;
