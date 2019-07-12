62460f0693a332eb33fdabdfb41b43e4ac24a53a
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-12 14:39:35 +0200

    [MenuButton] Collocate container and component

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -1,17 +1,24 @@
 // @flow
 import React from "react";
+import { connect } from "react-redux";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+
+import { openNavigation } from "../redux/navigation/actions";
 
 type Props = {
   className: string,
   openNavigation: Function
 };
 
+// eslint-disable-next-line no-shadow
 const MenuButton = ({ className, openNavigation }: Props) => (
   <IconButton onClick={openNavigation} className={className} color="inherit">
     <MenuIcon />
   </IconButton>
 );
 
-export default MenuButton;
+export default connect(
+  null,
+  { openNavigation }
+)(MenuButton);
