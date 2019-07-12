2b7f2871c6e09783c090fcc796d46b50b2f209a0
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-12 20:51:53 +0200

    [WelcomePage] Add

---
+++ src/components/pages/WelcomePage.js
@@ -0,0 +1,13 @@
+// @flow
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+import Layout from "../layouts/Layout";
+
+const WelcomePage = () => (
+  <Layout>
+    <Typography variant="h5">Welcome</Typography>
+  </Layout>
+);
+
+export default WelcomePage;
