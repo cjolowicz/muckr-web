7231291b9945692f147ee3ad8760b335ff33426c
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-13 17:56:43 +0200

    [redux] Add test for createStore

---
+++ src/redux/store/index.spec.js
@@ -0,0 +1,28 @@
+// @flow
+import Cookies from "universal-cookie";
+
+import createStore from ".";
+import rootReducer from "../reducers";
+import { message } from "../selectors";
+import { openMessage } from "../message/actions";
+
+const cookies = new Cookies();
+
+describe("createStore", () => {
+  describe("without preloadedState", () => {
+    it("uses the initial state", () => {
+      const store = createStore(cookies);
+      const state = store.getState();
+      expect(message(state)).toBeNull();
+    });
+  });
+
+  describe("with preloadedState", () => {
+    it("uses the preloaded state", () => {
+      const preloadedState = rootReducer(undefined, openMessage("Lorem"));
+      const store = createStore(cookies, preloadedState);
+      const state = store.getState();
+      expect(message(state)).toEqual("Lorem");
+    });
+  });
+});
