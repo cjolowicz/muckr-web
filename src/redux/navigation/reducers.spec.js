86e94976ad8bbe2a8ba7679c846a97fc3ead5e1d
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-13 22:20:34 +0200

    [redux] Minor cleanup in navigation reducer test

--- src/redux/navigation/reducers.spec.js
+++ src/redux/navigation/reducers.spec.js
@@ -1,27 +1,27 @@
 // @flow
-import navigation from "./reducers";
+import reducer from "./reducers";
 import { open } from "./selectors";
 import { openNavigation, closeNavigation } from "./actions";
 import { noop } from "../noop/actions";
 
-describe("navigation", () => {
+describe("reducer", () => {
   describe("initially", () => {
     it("is closed", () => {
-      const state = navigation(undefined, noop());
+      const state = reducer(undefined, noop());
       expect(open(state)).toBe(false);
     });
   });
 
   describe("openNavigation", () => {
     test.each([[false], [true]])("is open", open$ => {
-      const state = navigation({ open: open$ }, openNavigation());
+      const state = reducer({ open: open$ }, openNavigation());
       expect(open(state)).toBe(true);
     });
   });
 
   describe("closeNavigation", () => {
     test.each([[false], [true]])("is closed", open$ => {
-      const state = navigation({ open: open$ }, closeNavigation());
+      const state = reducer({ open: open$ }, closeNavigation());
       expect(open(state)).toBe(false);
     });
   });
