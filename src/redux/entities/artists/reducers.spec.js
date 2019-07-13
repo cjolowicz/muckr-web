93d3d5fb0e065ee094f5bfff5eaf4ac89f509e29
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-13 22:19:45 +0200

    [redux] Minor cleanup in entities/artists reducer test

--- src/redux/entities/artists/reducers.spec.js
+++ src/redux/entities/artists/reducers.spec.js
@@ -1,5 +1,5 @@
 // @flow
-import artists from "./reducers";
+import reducer from "./reducers";
 import {
   fetchArtistsSuccess,
   createArtistSuccess,
@@ -9,33 +9,33 @@ import {
 import { ARTIST, ARTISTS } from "../../../utils/test/fixtures";
 import indexById from "../../../utils/indexById";
 
-describe("artists", () => {
+describe("reducer", () => {
   const state = indexById([ARTIST]);
 
   it("inserts fetched artists", () => {
     const action = fetchArtistsSuccess(ARTISTS);
-    const result = artists(undefined, action);
+    const result = reducer(undefined, action);
     const expected = indexById(ARTISTS);
     expect(result).toEqual(expected);
   });
 
   it("inserts the created artist", () => {
     const action = createArtistSuccess(ARTIST);
-    const result = artists(undefined, action);
+    const result = reducer(undefined, action);
     const expected = indexById([ARTIST]);
     expect(result).toEqual(expected);
   });
 
   it("removes the artist with the specified ID", () => {
     const action = removeArtistSuccess(ARTIST.id);
-    const result = artists(state, action);
+    const result = reducer(state, action);
     expect(result).toEqual({});
   });
 
   it("updates the artist with the matching ID", () => {
     const artist = { ...ARTIST, name: "foobar" };
     const action = updateArtistSuccess(artist);
-    const result = artists(state, action);
+    const result = reducer(state, action);
     const expected = indexById([artist]);
     expect(result).toEqual(expected);
   });
