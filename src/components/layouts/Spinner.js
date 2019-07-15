ea2ef33e87b055a409466ceeffbb207afa4759d6
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-15 13:34:39 +0200

    [components] Add Spinner to layouts

---
+++ src/components/layouts/Spinner.js
@@ -0,0 +1,31 @@
+// @flow
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Fade from "@material-ui/core/Fade";
+
+import { spinner } from "../../redux/selectors";
+
+type Props = {
+  className: string,
+  spinner: boolean
+};
+
+// eslint-disable-next-line no-shadow
+export const Spinner = ({ className, spinner }: Props) => (
+  <Fade
+    in={spinner}
+    style={{
+      transitionDelay: spinner ? "800ms" : "0ms"
+    }}
+    unmountOnExit
+  >
+    <CircularProgress className={className} color="secondary" size={30} />
+  </Fade>
+);
+
+export default connect(
+  createStructuredSelector({ spinner }),
+  null
+)(Spinner);
