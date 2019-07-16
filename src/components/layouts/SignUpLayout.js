8c4326aeb0a0da5edc6cef60f75ef76e5619192f
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-16 12:56:12 +0200

    [components] Move Paper from SignUp to SignUpLayout

--- src/components/layouts/SignUpLayout.js
+++ src/components/layouts/SignUpLayout.js
@@ -1,5 +1,7 @@
 // @flow
 import * as React from "react";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/styles";
 
 import { Header } from "./Layout";
 import Message from "./Message";
@@ -8,13 +10,36 @@ type Props = {
   children: React.Node
 };
 
+const useStyles = makeStyles(theme => ({
+  main: {
+    width: "auto",
+    marginLeft: theme.spacing(3),
+    marginRight: theme.spacing(3),
+    [theme.breakpoints.up(400 + theme.spacing(3 * 2))]: {
+      width: 400,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: theme.spacing(2, 3, 3)
+  }
+}));
+
 const SignUpLayout = ({ children }: Props) => {
+  const classes = useStyles();
   return (
-    <>
-      <Header />
-      {children}
-      <Message />
-    </>
+    <main className={classes.main}>
+      <Paper className={classes.paper}>
+        <Header />
+        {children}
+        <Message />
+      </Paper>
+    </main>
   );
 };
 
