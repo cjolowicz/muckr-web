7eeb6adbcdec459900173d2f13bd0d1a2710bafa
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-20 21:51:01 +0200

    [redux] Do not reset message on success
    Do not reset message when user is signed in or when artists are fetched. There
    is no need to reset messages outside of the CLOSE_MESSAGE action.

--- src/redux/message/reducers.js
+++ src/redux/message/reducers.js
@@ -1,12 +1,11 @@
 // @flow
 import { OPEN_MESSAGE, CLOSE_MESSAGE } from "./constants";
-import { FETCH_TOKEN_SUCCESS, FETCH_TOKEN_FAILURE } from "../token/constants";
+import { FETCH_TOKEN_FAILURE } from "../token/constants";
 import { CREATE_USER_SUCCESS, CREATE_USER_FAILURE } from "../user/constants";
 import {
   CREATE_ARTIST_FAILURE,
   REMOVE_ARTIST_FAILURE,
   UPDATE_ARTIST_FAILURE,
-  FETCH_ARTISTS_SUCCESS,
   FETCH_ARTISTS_FAILURE
 } from "../artist/constants";
 import type { Action } from "../types";
@@ -51,8 +50,6 @@ const message = (state: ?string = null, action: Action) => {
       return action.payload.message;
     case CREATE_USER_SUCCESS:
       return "Account created";
-    case FETCH_TOKEN_SUCCESS:
-    case FETCH_ARTISTS_SUCCESS:
     case CLOSE_MESSAGE:
       return null;
     default:
